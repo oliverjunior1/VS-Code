5d5e423de0f340c563566b53378ff2d14b536d91
oliverjunior1 <oliver-junior@hotmail.com>
2025-06-12 23:04:06 -0300

    Update 2_calculo_imc.js

--- DIo/1_Javascript/2_calculo_imc.js
+++ DIo/1_Javascript/2_calculo_imc.js
@@ -1,7 +1,20 @@
 let weight = Number(prompt("Type your weight: "))
 let height = Number(prompt("Type your height: "))
 
-let imc = weight/(height**2)
+let imc = weight / (height ** 2)
 
-console.log(imc)
-alert(imc)
+if (imc < 18.5) {
+    alert("low weight")
+}
+else if (imc >= 18.5 && imc < 25) {
+    alert("normal weight")
+}
+else if (imc >= 25 && imc < 30) {
+    alert("overweight")
+}
+else if (imc >= 30 && imc < 35) {
+    alert("obesity")
+}
+else {
+    alert("severe obesity")
+}
