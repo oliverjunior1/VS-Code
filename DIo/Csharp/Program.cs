7b953398f0d760370c1e9a4b63ce26d33fe60145
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-16 21:18:32 -0300

    Update Program.cs

--- DIo/Csharp/Program.cs
+++ DIo/Csharp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Csharp.Models;
+
+Pessoa p = new Pessoa();
+p.Nome = "Joaquim";
+p.Idade = 42;
+p.Apresentar();
