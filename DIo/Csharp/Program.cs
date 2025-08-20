0336c004f27bffe7c0ced091f543970a5b893d02
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-20 18:22:09 -0300

    Update Program.cs

--- DIo/Csharp/Program.cs
+++ DIo/Csharp/Program.cs
@@ -1,6 +1,9 @@
-﻿
+﻿int i = 12;
 double d = 765.12;
+float f = 45.123F;
 
-int i = (int)d;
-
-Console.WriteLine("Value of i is " + i);
+Console.WriteLine(Convert.ToString(f));
+Console.WriteLine(Convert.ToInt32(d));
+Console.WriteLine(Convert.ToUInt32(f));
+Console.WriteLine(Convert.ToDouble(i));
+Console.WriteLine("GeeksforGeeks");
