a81b7d86485d02a44d1d7d8752c65410b17a5281
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-23 16:29:15 -0300

    Atualizacao código treinamento
    Treinamento sobre switch, com input.

--- DIo/Csharp - Copia/Program.cs
+++ DIo/Csharp - Copia/Program.cs
@@ -1,42 +1,35 @@
 ﻿while (true)
 {
-    Console.WriteLine("Put the number to see the day: ");
-    string day_string = Console.ReadLine();
-    int day = Convert.ToInt32(day_string);
-    switch (day)
+    Console.WriteLine("Put the number of the day to see the week day correspondently: ");
+    string day = Console.ReadLine();
+    int day_num = Convert.ToInt32(day);
+
+    switch (day_num)
     {
         case 1:
             Console.WriteLine("Sunday");
             break;
-        
         case 2:
-            Console.WriteLine("Monady");
+            Console.WriteLine("Monday");
             break;
-        
         case 3:
             Console.WriteLine("Tuesday");
             break;
-        
         case 4:
             Console.WriteLine("Wednesday");
             break;
-        
         case 5:
             Console.WriteLine("Thursday");
             break;
-        
         case 6:
             Console.WriteLine("Friday");
             break;
-        
         case 7:
             Console.WriteLine("Saturday");
             break;
-        
-        
         default:
-            Console.WriteLine("Invalid number!");
+            Console.WriteLine("Opcao invalida.");
             break;
-        
     }
+
 }
