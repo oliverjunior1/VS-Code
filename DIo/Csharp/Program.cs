bdde55e03239b68d24f52f83e136f2a43d704a56
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-22 22:01:02 -0300

    Update Program.cs

--- DIo/Csharp/Program.cs
+++ DIo/Csharp/Program.cs
@@ -1,18 +1,3 @@
-﻿
+﻿int x = 5 + 15;
 
-while (true)
-
-{
-    Console.WriteLine("Put your age: ");
-    string ageString = Console.ReadLine();
-    int age = Convert.ToInt32(ageString);
-    if (age >= 18 && age<=70)
-    {
-        Console.WriteLine("You can drive, pass away!");
-    }
-    else if(age < 18 && age > 70)
-    {
-        Console.WriteLine("You can't drive. Call someone.");
-    }
-
-}
+Console.WriteLine(x);
