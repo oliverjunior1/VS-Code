6557cbfab124df3c9815ee07fc179bfc16ef3140
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-23 20:02:16 -0300

    Treinamento de listas e arrays
    Muito boa aula de arrays e listas em C#.

--- DIo/Csharp/Program.cs
+++ DIo/Csharp/Program.cs
@@ -5,24 +5,58 @@
 //     Console.WriteLine($"The array {i} é {array_test[i]}");
 // }
 ///////////////////////////////////////////////////////////////
-int[] arrayInteiros = new int[3];
+// int[] arrayInteiros = new int[3];
 
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
-// arrayInteiros[3] = 1;
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// // arrayInteiros[3] = 1;
 
-// Percorrendo o Array com o For
-for (int i = 0; i < arrayInteiros.Length; i++)
+// // Percorrendo o Array com o For
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     Console.WriteLine($"Posicao nº {i} - {arrayInteiros[i]}");
+// }
+
+// // Console.WriteLine("Percorrendo o array com o foreach");
+// // int contadorForEach = 0;
+// // foreach (int valor in arrayInteiros)
+// // {
+// //     Console.WriteLine($"Posicao nº{contadorForEach}-{valor}");
+// //     contadorForEach++;
+// // }
+// Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+// int[] arratInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arratInteirosDobrado, arrayInteiros.Length);
+/////////////////////////////Listas//////////////////////////////
+
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("GO");
+
+for (int i = 0; i < listaString.Count; i++)
 {
-    Console.WriteLine($"Posicao nº {i} - {arrayInteiros[i]}");
+    Console.WriteLine($"Posicao nº {i} - {listaString[i]}");
 }
 
-// Console.WriteLine("Percorrendo o array com o foreach");
-// int contadorForEach = 0;
-// foreach (int valor in arrayInteiros)
-// {
-//     Console.WriteLine($"Posicao nº{contadorForEach}-{valor}");
-//     contadorForEach++;
-// }
-Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+int contador = 0;
+foreach (string item in listaString)
+{
+    Console.WriteLine($"Posicao nº {contador} - {listaString[contador]}");
+    contador++;
+}
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("MG");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
