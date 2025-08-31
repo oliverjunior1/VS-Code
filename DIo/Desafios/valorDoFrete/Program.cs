1d158b11ef1b3500902e563164af1270e0761af4
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-31 12:12:46 -0300

    Create Program.cs

---
+++ DIo/Desafios/valorDoFrete/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class DescontoInteligente
+{
+    static void Main()
+    {
+
+        // Lê o valor original do produto
+        double valorOriginal = Convert.ToDouble(Console.ReadLine());
+
+        // Lê a porcentagem de desconto
+        double porcentagemDesconto = Convert.ToDouble(Console.ReadLine());
+
+        // TODO: Verifique se o desconto está dentro de um intervalo válido
+
+        // TODO: Calcule o valor final do produto
+        if (porcentagemDesconto > 100)
+        {
+            Console.WriteLine("Desconto invalido");
+        }
+        else
+        {
+            double valorFinal = valorOriginal * ((-porcentagemDesconto + 100) / 100);
+            // Exibe o valor com duas casas decimais
+            Console.WriteLine($"{valorFinal:F2}");
+        }
+
+
+
+
+
+    }
+}
