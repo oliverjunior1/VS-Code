d37dd657eddadb7418ade6c32d86cfcb659cc3e7
oliverjunior1 <oliver-junior@hotmail.com>
2025-11-11 20:15:29 -0300

    Create 12_DIO_3.sql

---
+++ SQL Udemy Jose Portilla/12_DIO_3.sql
@@ -0,0 +1,6 @@
+SELECT * FROM actor c LIMIT 10;
+SELECT COUNT(*) FROM address c2;
+SELECT address, COUNT(*) AS Total FROM address GROUP BY 1 ORDER BY Total DESC LIMIT 10;
+
+SELECT * FROM clientes;
+SELECT COUNT(*) FROM customer;
