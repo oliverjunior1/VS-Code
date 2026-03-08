bb21af2a434a8f227ddb760a107e9c6be049bf3a
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-08 17:36:04 -0300

    Update 17_Challenge_one.sql

--- SQL Udemy Jose Portilla/17_Challenge_one.sql
+++ SQL Udemy Jose Portilla/17_Challenge_one.sql
@@ -1,17 +1,17 @@
 -- How many payment transactions were greater than $5.00?
-SELECT * FROM payment WHERE amount >= 5;
+
 
 -- How many actors have the fist name thats starts  with the letter P?
-SELECT COUNT(*) FROM actor WHERE first_name LIKE 'P%';
+
 
 -- How many unique districts are our customers from?
-SELECT COUNT(DISTINCT address_id) FROM customer;
+
 
 -- Retrieve the list of names for those distinct districts from the previous question.
-SELECT DISTINCT address_id FROM customer;
+
 
 -- How many films have a rating of R and a replacement cost between $5 and $15?
-SELECT COUNT(*) FROM film WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15;
+
 
 -- How many films have the word Truman womewhere in the title?
-SELECT COUNT(title) FROM film WHERE title LIKE '%Truman%';
+
