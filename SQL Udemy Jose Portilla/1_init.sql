2815ca37135eedf673bf02384e827748b3adc6d3
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-02 19:30:23 -0300

    Create 1_init.sql

---
+++ SQL Udemy Jose Portilla/1_init.sql
@@ -0,0 +1,12 @@
+SELECT * 
+FROM clientes;
+
+CREATE TABLE clientes (
+	id INTEGER,
+	nome TEXT,
+	idade INTEGER
+);
+
+INSERT INTO clientes VALUES (1, Ana, 25);
+INSERT INTO clientes VALUES (2, Pedro, 30);
+INSERT INTO clientes VALUES (3, Ana, 22);
