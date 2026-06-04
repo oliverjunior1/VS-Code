9ef6086c32a202d27f0f8ced1c0a3bf7de856c87
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-04 20:54:36 -0300

    Create 4_exercicio_group_by.sql

---
+++ SQL Udemy Jose Portilla/4_exercicio_group_by.sql
@@ -0,0 +1,13 @@
+/*CREATE TABLE funcionarios (
+    departamento VARCHAR(50),
+    salario DECIMAL(10,2)
+);*/
+/*
+INSERT INTO funcionarios (departamento, salario)
+VALUES
+('TI', 5000),
+('TI', 7000),
+('RH', 4000),
+('RH', 6000),
+('Financeiro', 8000);*/
+
