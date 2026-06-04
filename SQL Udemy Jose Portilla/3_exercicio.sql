ade8afbeea1ff998ba7c4c7efacba3b5490e7469
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-04 19:44:44 -0300

    Update 3_exercicio.sql

--- SQL Udemy Jose Portilla/3_exercicio.sql
+++ SQL Udemy Jose Portilla/3_exercicio.sql
@@ -1,4 +1,20 @@
-CREATE TABLE vendas_produtos (
+/*CREATE TABLE vendas_produtos (
     produto VARCHAR(50),
     vendas INT
-);
+);*/
+/*
+INSERT INTO vendas_produtos (produto, vendas)
+VALUES
+('Mouse', 100),
+('Mouse', 150),
+('Teclado', 200),
+('Monitor', 500),
+('Monitor', 300);*/
+
+-- 1. Total vendido por produto.
+--SELECT SUM(vendas) FROM vendas_produtos;
+/*SELECT produto,
+       SUM(vendas) AS total_vendido
+FROM vendas_produtos
+GROUP BY produto;*/
+
