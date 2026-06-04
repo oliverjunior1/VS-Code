31bee2cf7b935da112b57d8b6b3bebff8fd7fb65
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-03 22:14:45 -0300

    Update 5_lista_de_dicionarios.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/5_lista_de_dicionarios.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/5_lista_de_dicionarios.py
@@ -4,5 +4,5 @@ funcionarios = [
     {"nome": "Julia", "salario": 4000}
 ]
 
-maior = max(funcionarios, key=lambda x:x['salario'])
+maior = max(funcionarios, key= lambda x:x['salario'])
 print(maior)
