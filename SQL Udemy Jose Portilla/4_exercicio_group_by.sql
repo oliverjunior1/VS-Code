848299ba6cafe056e64da99c7af7dc3659315e7f
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-04 21:15:10 -0300

    Update 4_exercicio_group_by.sql

--- SQL Udemy Jose Portilla/4_exercicio_group_by.sql
+++ SQL Udemy Jose Portilla/4_exercicio_group_by.sql
@@ -12,6 +12,14 @@ VALUES
 ('Financeiro', 8000);*/
 
 --1. Média salarial por departamento.
-SELECT departamento, AVG(salario) FROM funcionarios GROUP BY departamento;
+--SELECT departamento, AVG(salario) FROM funcionarios GROUP BY departamento;
+
 --2. Total salarial por departamento.
+--SELECT departamento,SUM(salario) FROM funcionarios GROUP BY departamento;
+
 --3. Departamentos com média acima de 5500.
+SELECT departamento,
+       AVG(salario) AS media_salarial
+FROM funcionarios
+GROUP BY departamento
+HAVING AVG(salario) > 5500;
