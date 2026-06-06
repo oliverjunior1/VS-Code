8bb243869eb1fb71aba52cc32b0c6b018e2ddf0e
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 18:15:48 -0300

    Update 11_sorted.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/11_sorted.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/11_sorted.py
@@ -1,6 +1,16 @@
-salarios = [5000, 3000, 7000, 4000]
+""" salarios = [5000, 3000, 7000, 4000]
 
 # salarios_ordenados = sorted(salarios)
 salarios_ordenados = sorted(salarios, reverse=True)
 
-print(salarios_ordenados)
+print(salarios_ordenados) """
+
+funcionarios = [
+    {"nome":"Ana","salario":5000},
+    {"nome":"Pedro","salario":7000},
+    {"nome":"Julia","salario":4000}
+]
+
+sorted_dict = sorted(funcionarios, key=lambda x:x['salario'])
+
+print(sorted_dict)
