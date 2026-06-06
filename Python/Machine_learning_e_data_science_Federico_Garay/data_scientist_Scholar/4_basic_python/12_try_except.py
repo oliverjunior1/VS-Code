f1a2c836fbe84d037951b7d450828707eb7e1dcb
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 18:27:35 -0300

    Update 12_try_except.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/12_try_except.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/12_try_except.py
@@ -1,4 +1,9 @@
-try:
+""" try:
     x = 10/0
 except:
-    print("Any nunber can't be divide for zero!")
+    print("Any nunber can't be divide for zero!") """
+
+try:
+    numero = int(input("Digite um número: "))
+except ValueError:
+    print("Digite apenas números.")
