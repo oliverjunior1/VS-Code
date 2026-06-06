4d871e47ac0ce5dbab7b466b7bddcd0569d07014
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 16:42:49 -0300

    Update 9_exercicios_lambda_e_map.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/9_exercicios_lambda_e_map.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/9_exercicios_lambda_e_map.py
@@ -1,5 +1,15 @@
-idades = [18,20,25,30] #adicione um a todos os números da lista:
+""" idades = [18,20,25,30] #adicione um a todos os números da lista:
 
 idades_plus_1 = [x+1 for x in idades]
 
+print(idades_plus_1) """
+
+# usando map
+idades = [18,20,25,30]
+
+idades_plus_1 = list(map(lambda x:x+1, idades))
+
 print(idades_plus_1)
+
+
+
