648f12a9205d02c36112f0ac96d23cb903852425
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 19:10:41 -0300

    Update 14_classes.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/14_classes.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/14_classes.py
@@ -1,4 +1,4 @@
-class Funcionario:
+""" class Funcionario:
     def __init__(self):
         self.nome = nome
         self.salario = salario
@@ -6,4 +6,19 @@ class Funcionario:
 
 Ana = Funcionario('Ana', 5000)
 
-print(Ana)
+print(Ana) """
+
+funcionarios = [
+    {"nome":"Ana","salario":5000},
+    {"nome":"Pedro","salario":7000},
+    {"nome":"Julia","salario":4000},
+    {"nome":"Carlos","salario":6000}
+]
+
+# Maior salário.
+maior_salario = sorted(funcionarios, key=lambda a:a['salario'])
+print(maior_salario[-1])
+# Menor salário.
+# Média salarial.
+# Funcionários acima da média.
+# Funcionários ordenados por salário.
