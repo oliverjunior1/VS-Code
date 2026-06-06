60e0c2fc7b4d593cf5c6ea0ad6213fbbef3e252d
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 17:35:29 -0300

    Update 11_sorted.py

--- Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/11_sorted.py
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist_Scholar/4_basic_python/11_sorted.py
@@ -1,5 +1,6 @@
 salarios = [5000, 3000, 7000, 4000]
 
-salarios_ordenados = sorted(salarios)
+# salarios_ordenados = sorted(salarios)
+salarios_ordenados = sorted(salarios, reverse=True)
 
 print(salarios_ordenados)
