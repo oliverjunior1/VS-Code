b4b62329423903dd6b1f2010a62140a7da497d29
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-06 21:47:45 -0300

    Update 5_explorar_dados.sql

--- SQL Udemy Jose Portilla/5_explorar_dados.sql
+++ SQL Udemy Jose Portilla/5_explorar_dados.sql
@@ -3,8 +3,12 @@
 -- 	valor INT
 -- );
 
-INSERT INTO produtos (produto, valor)
-VALUES
-('Mouse', 100),
-('Mouse',150),
-('Monitor', 500);
+-- INSERT INTO produtos (produto, valor)
+-- VALUES
+-- ('Mouse', 100),
+-- ('Mouse',150),
+-- ('Monitor', 500);
+
+-- o total vendido por produto.
+
+SELECT produto, SUM(valor) AS total_vendido FROM produtos GROUP BY produto;
