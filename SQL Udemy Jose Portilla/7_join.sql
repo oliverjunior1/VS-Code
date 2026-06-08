b69d140c1348373e56ffd3dc03ee92aa9246854f
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-08 18:22:31 -0300

    Update 7_join.sql

--- SQL Udemy Jose Portilla/7_join.sql
+++ SQL Udemy Jose Portilla/7_join.sql
@@ -11,8 +11,14 @@
 
 -- SELECT * FROM clientes_novo;
 
-CREATE TABLE pedidos(
-pedido_id INT PRIMARY KEY,
-cliente_id INT,
-valor INT
-);
+-- CREATE TABLE pedidos(
+-- pedido_id INT PRIMARY KEY,
+-- cliente_id INT,
+-- valor INT
+-- );
+
+INSERT INTO pedidos (pedido_id, cliente_id, valor)
+VALUES
+(101,1,100),
+(102,1,200),
+(103,2,300);
