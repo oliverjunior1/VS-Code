f03fd3344df4af0d5d827f8ee92fe0e6e9b3a4fa
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-08 18:12:05 -0300

    Create 7_join.sql

---
+++ SQL Udemy Jose Portilla/7_join.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE clientes_novo(
+-- 	clientes_id INT PRIMARY KEY,
+-- 	nome VARCHAR(100)
+-- );
+
+INSERT INTO clientes_novo (clientes_id, nome)
+VALUES
+	(1, 'Ana'),
+	(2, 'Pedro'),
+	(3, 'Julia');
