a119d699868c844efa6b291ee0332f5ac39c3a47
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-09 19:34:51 -0300

    Create 2_create_table.sql

---
+++ SQL Udemy Jose Portilla/Curso_SQL_do_zero_ao_avançado/2_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	data_nascimento DATE NOT NULL,
+	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
