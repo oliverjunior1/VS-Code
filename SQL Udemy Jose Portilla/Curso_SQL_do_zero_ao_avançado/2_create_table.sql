bd249296ceedc997878e64817678ef07881086e9
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-09 19:41:48 -0300

    Update 2_create_table.sql

--- SQL Udemy Jose Portilla/Curso_SQL_do_zero_ao_avançado/2_create_table.sql
+++ SQL Udemy Jose Portilla/Curso_SQL_do_zero_ao_avançado/2_create_table.sql
@@ -1,12 +1,14 @@
--- CREATE TABLE usuarios(
--- 	id SERIAL PRIMARY KEY,
--- 	nome VARCHAR(50) NOT NULL,
--- 	email VARCHAR(50) NOT NULL,
--- 	data_nascimento DATE NOT NULL,
--- 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE usuarios(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	data_nascimento DATE NOT NULL,
+	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
--- );
+);
 
-INSERT INTO usuarios(nome, email, data_nascimento)
-VALUES('Marcos Duarte', 'marcos@gmail.com', '06/07/1994');
+-- INSERT INTO usuarios(nome, email, data_nascimento)
+-- VALUES('Marcos Duarte', 'marcos@gmail.com', '06/07/1994');
+
+-- SELECT * FROM usuarios;
 
