86db13f02a51a6224ac8dd2bbc57bc1510bdc903
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-10 19:16:41 -0300

    Update 20_group_by.sql

--- SQL Udemy Jose Portilla/20_group_by.sql
+++ SQL Udemy Jose Portilla/20_group_by.sql
@@ -11,8 +11,12 @@
  -- GROUP BY staff_id, customer_id
  -- ORDER BY staff_id, customer_id;
 
+ -- SELECT staff_id, customer_id, SUM(amount) FROM payment
+ -- GROUP BY staff_id, customer_id
+ -- ORDER BY customer_id;
+ 
  SELECT staff_id, customer_id, SUM(amount) FROM payment
  GROUP BY staff_id, customer_id
- ORDER BY customer_id;
+ ORDER BY SUM(amount);
 
  
