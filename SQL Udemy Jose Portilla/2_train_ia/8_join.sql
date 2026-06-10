ccae81ca1e90cd4e83c64063561006b5819bbfea
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-10 20:36:38 -0300

    Update 8_join.sql

--- SQL Udemy Jose Portilla/2_train_ia/8_join.sql
+++ SQL Udemy Jose Portilla/2_train_ia/8_join.sql
@@ -1,5 +1,13 @@
 -- Criar a tabela
-CREATE TABLE clientes (
-	cliente_id INT PRIMARY KEY,
-	nome VARCHAR(100)
-);
+-- CREATE TABLE clientes (
+-- 	cliente_id INT PRIMARY KEY,
+-- 	nome VARCHAR(100)
+-- );
+
+-- Inserir os registros
+-- INSERT INTO clientes (cliente_id, nome)
+-- VALUES
+-- (1,'Ana'),
+-- (2,'Pedro'),
+-- (3,'Julia');
+
