6641d932cfa2cc2f9e540f70219d1d9a8ec5b8e8
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-10 20:17:44 -0300

    Create 5_explorar_dados.sql

---
+++ SQL Udemy Jose Portilla/2_train_ia/5_explorar_dados.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE produtos (
+-- 	produto VARCHAR(50),
+-- 	valor INT
+-- );
+
+-- INSERT INTO produtos (produto, valor)
+-- VALUES
+-- ('Mouse', 100),
+-- ('Mouse',150),
+-- ('Monitor', 500);
+
+-- o total vendido por produto.
+
+-- SELECT produto, SUM(valor) AS total_vendido 
+-- FROM produtos GROUP BY produto;
+
