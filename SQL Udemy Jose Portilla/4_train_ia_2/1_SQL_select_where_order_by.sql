b3b47b326739d1d65e14b6db7e76bf61b69553e3
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-15 20:58:34 -0300

    Update 1_SQL_select_where_order_by.sql

--- SQL Udemy Jose Portilla/4_train_ia_2/1_SQL_select_where_order_by.sql
+++ SQL Udemy Jose Portilla/4_train_ia_2/1_SQL_select_where_order_by.sql
@@ -7,3 +7,10 @@
 -- 	compras INT
 -- );
 
+-- Inserir dados
+-- INSERT INTO clientes (id, nome, idade, cidade, compras) VALUES
+-- (1,'Ana', 22, 'São Paulo',3),
+-- (2,'Bruno', 35, 'Rio de Janeiro',8),
+-- (3,'Carla', 29, 'São Paulo',5),
+-- (4,'Diego', 41, 'Recife',2),
+-- (5,'Eva', 30, 'Curitiba',10);
