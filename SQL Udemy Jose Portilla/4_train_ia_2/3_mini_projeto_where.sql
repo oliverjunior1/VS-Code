0e5671ef6b6a96b04e8e9bc58e4419cd861a3b8e
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 22:05:23 -0300

    Update 3_mini_projeto_where.sql

--- SQL Udemy Jose Portilla/4_train_ia_2/3_mini_projeto_where.sql
+++ SQL Udemy Jose Portilla/4_train_ia_2/3_mini_projeto_where.sql
@@ -1,8 +1,8 @@
 -- Criar tabela
-CREATE TABLE produtos(
-	id INT PRIMARY KEY,
-	produto VARCHAR(50),
-	categoria VARCHAR(50),
-	preco DECIMAL(10,2),
-	quantidade INT
-);
+-- CREATE TABLE produtos(
+-- 	id INT PRIMARY KEY,
+-- 	produto VARCHAR(50),
+-- 	categoria VARCHAR(50),
+-- 	preco DECIMAL(10,2),
+-- 	quantidade INT
+-- );
