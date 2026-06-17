d29ad63553a15058d147edb62088b9d480fa22b2
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 21:59:10 -0300

    Update 2_exercicio_guiado_where.sql

--- SQL Udemy Jose Portilla/4_train_ia_2/2_exercicio_guiado_where.sql
+++ SQL Udemy Jose Portilla/4_train_ia_2/2_exercicio_guiado_where.sql
@@ -15,6 +15,6 @@
 -- FROM clientes GROUP BY nome;
 
 -- Mostrar a maior quantidade de compras com o nome correspondente
-SELECT nome, compras FROM clientes 
-WHERE compras = (SELECT MAX(compras) 
-FROM clientes);
+-- SELECT nome, compras FROM clientes 
+-- WHERE compras = (SELECT MAX(compras) 
+-- FROM clientes);
