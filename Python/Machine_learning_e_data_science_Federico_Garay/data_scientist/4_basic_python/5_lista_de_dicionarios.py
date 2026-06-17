102d2728794c8416376696ce39e553d935d19dee
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 22:52:23 -0300

    Create 5_lista_de_dicionarios.py

---
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist/4_basic_python/5_lista_de_dicionarios.py
@@ -0,0 +1,8 @@
+funcionarios = [
+    {"nome": "Ana", "salario": 5000},
+    {"nome": "Pedro", "salario": 7000},
+    {"nome": "Julia", "salario": 4000}
+]
+
+maior = max(funcionarios, key= lambda x:x['salario'])
+print(maior)
