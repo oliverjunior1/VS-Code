9fe8e8b0224889ca11183955d54304f341734ef0
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 22:51:50 -0300

    Create 12_try_except.py

---
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist/4_basic_python/12_try_except.py
@@ -0,0 +1,20 @@
+""" try:
+    x = 10/0
+except:
+    print("Any nunber can't be divide for zero!") """
+
+""" try:
+    numero = int(input("Digite um número: "))
+except ValueError:
+    print("Digite apenas números.") """
+
+# Exercício 4
+# Crie um programa que:
+# peça salário
+# trate erros de digitação
+
+""" try:
+    salario = float(input("Type your salary: "))
+except:
+    print('You need to put a number.') """
+
