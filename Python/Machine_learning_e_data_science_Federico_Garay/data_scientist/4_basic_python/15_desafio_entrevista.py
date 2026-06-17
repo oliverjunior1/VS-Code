24d5b289cda978192f453148d5e01dfc5bf81486
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 22:52:02 -0300

    Create 15_desafio_entrevista.py

---
+++ Python/Machine_learning_e_data_science_Federico_Garay/data_scientist/4_basic_python/15_desafio_entrevista.py
@@ -0,0 +1,18 @@
+import pandas as pd
+vendas = [100, 200, 300, 400, 500]
+sr = pd.Series(vendas)
+# Calcule total.
+print(sr.sum())
+
+# Calcule média.
+print(sr.mean())
+
+# Filtre vendas acima da média.
+print(sr[sr>sr.mean()])
+
+# Ordene do maior para o menor.
+print(sorted(vendas, reverse=True))
+
+# Aplique desconto de 10%.
+desconto_10 = [x*0.9 for x in vendas]
+print(desconto_10)
