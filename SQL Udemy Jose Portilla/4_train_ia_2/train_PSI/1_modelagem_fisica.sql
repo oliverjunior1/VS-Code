3a63f71e9680f2b90173d35808140efb935b0686
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-17 15:04:18 -0300

    Update 1_modelagem_fisica.sql

--- SQL Udemy Jose Portilla/4_train_ia_2/train_PSI/1_modelagem_fisica.sql
+++ SQL Udemy Jose Portilla/4_train_ia_2/train_PSI/1_modelagem_fisica.sql
@@ -18,8 +18,17 @@
 --     email VARCHAR(100)
 -- );
 
-CREATE TABLE produto (
-    id_produto INT PRIMARY KEY,
-    nome VARCHAR(100),
-    preco DECIMAL(10,2)
-);
+-- CREATE TABLE produto (
+--     id_produto INT PRIMARY KEY,
+--     nome VARCHAR(100),
+--     preco DECIMAL(10,2)
+-- );
+
+-- CREATE TABLE pedido (
+--     id_pedido INT PRIMARY KEY,
+--     data_pedido DATE,
+--     id_cliente INT,
+--     FOREIGN KEY(id_cliente)
+--     REFERENCES cliente(id_cliente)
+-- );
+
