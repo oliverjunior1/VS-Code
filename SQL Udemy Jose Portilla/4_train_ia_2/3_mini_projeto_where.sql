ea7cc16c84ebf0bf19aff01352533d619917e110
oliverjunior1 <oliver-junior@hotmail.com>
2026-06-16 22:18:41 -0300

    Update 3_mini_projeto_where.sql

--- SQL Udemy Jose Portilla/4_train_ia_2/3_mini_projeto_where.sql
+++ SQL Udemy Jose Portilla/4_train_ia_2/3_mini_projeto_where.sql
@@ -6,3 +6,10 @@
 -- 	preco DECIMAL(10,2),
 -- 	quantidade INT
 -- );
+
+-- INSERT INTO produtos (id, produto, categoria, preco, quantidade) VALUES
+-- (1, 'Mouse', 'Informática', 50.00,2),
+-- (2, 'Teclado', 'Informática', 120.00,1),
+-- (3, 'Caderno', 'Papelaria', 25.00,4),
+-- (4, 'Caneta', 'Papelaria', 5.00,10),
+-- (5, 'Monitor', 'Informática', 900.00,1);
