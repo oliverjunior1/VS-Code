d817f95f4bc85213ed181dac60b2c725d05cbb0c
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-09 16:26:09 -0300

    fix the name of the folder

--- DIo/Csharp/obj/Debug/net9.0/Csharp.AssemblyInfo.cs
+++ DIo/Csharp/obj/Debug/net9.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bc235682a0f0ac21d511905459ad686fedc8dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b232e44a31c48644089e97f656440098192815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
