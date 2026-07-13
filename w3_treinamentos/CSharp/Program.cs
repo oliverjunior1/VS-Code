e6cdf5b91084e21569cb61440005e807c79316d5
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-13 19:41:57 -0300

    Javascript if, else

--- w3_treinamentos/CSharp/Program.cs
+++ w3_treinamentos/CSharp/Program.cs
@@ -23,11 +23,11 @@
 // Console.WriteLine(myDouble);
 // Console.WriteLine(myInt);
 
-int myInt = 10;
-double myDouble = 5.25;
-bool myBool = true;
+// int myInt = 10;
+// double myDouble = 5.25;
+// bool myBool = true;
+// Console.WriteLine(Convert.ToString(myInt));
+// Console.WriteLine(Convert.ToDouble(myInt));
+// Console.WriteLine(Convert.ToInt32(myDouble));
+// Console.WriteLine(Convert.ToString(myBool));
 
-Console.WriteLine(Convert.ToString(myInt));
-Console.WriteLine(Convert.ToDouble(myInt));
-Console.WriteLine(Convert.ToInt32(myDouble));
-Console.WriteLine(Convert.ToString(myBool));
---
+++ w3_treinamentos/javascript/2_if_else/index.js
@@ -0,0 +1,4 @@
+let hour = 5;
+if (hour<18) {
+    alert("Good day");
+}
