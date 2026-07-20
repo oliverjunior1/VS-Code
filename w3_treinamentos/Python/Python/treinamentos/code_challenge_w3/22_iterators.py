2414622ded6fb0ba73fa136c10848ce8e53eda23
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:11 -0300

    Create 22_iterators.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/22_iterators.py
@@ -0,0 +1,10 @@
+'''Create a tuple called mytuple with the values "apple", "banana", "cherry"
+Create a variable myit and use iter() on the tuple
+Print the first item using next()'''
+
+# Create a tuple
+mytuple = ("apple", "banana", "cherry")
+# Create an iterator
+myit = iter(mytuple)
+# Print the first item
+print(next(myit))
