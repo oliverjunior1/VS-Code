97169042c9c23892d6938a01c5c3f58bd82ccb24
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:06 -0300

    Create 20_range.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/20_range.py
@@ -0,0 +1,11 @@
+'''Inside the editor, complete the following steps:
+Use a for loop with range(6) to print numbers 0 through 5
+Use a for loop with range() to print numbers 2 through 5'''
+
+# Print 0 through 5
+for x in range(6):
+    print(x)
+
+# Print 2 through 5
+for x in range(2,6):
+    print(x)
