fab3a2d44e139ccac93aefe25c4ec30eef651b69
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:02 -0300

    Create 19_function_basics.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/19_function_basics.py
@@ -0,0 +1,11 @@
+'''Create a function called greet
+Add a parameter called name to the function
+Inside the function, print "Hello, " followed by the name parameter
+Call the function with the argument "Emil"'''
+
+# Create the greet function
+def greet(name):
+    print(f"Hello {name}")
+
+# Call greet
+greet("Emil")
