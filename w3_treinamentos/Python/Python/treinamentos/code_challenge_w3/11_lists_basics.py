407694d6c3b748d3082c618b32df348a3db8b0af
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:53:39 -0300

    Create 11_lists_basics.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/11_lists_basics.py
@@ -0,0 +1,13 @@
+# Create a list
+colors = ["red", "green", "blue"]
+# Print the first item
+print(colors[0])
+# Change the second item to "yellow"
+colors[1] = 'yellow'
+print(colors[1])
+# Add "purple" to the end
+colors.append('purple')
+# Remove "red"
+colors.remove('red')
+# Print the list
+print(colors)
