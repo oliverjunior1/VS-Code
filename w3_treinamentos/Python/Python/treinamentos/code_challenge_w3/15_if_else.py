2ddfb40a53495bc48a5cc789808a1c6e46471523
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:53:49 -0300

    Create 15_if_else.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/15_if_else.py
@@ -0,0 +1,14 @@
+'''Create a variable age with the value 20
+Write an if statement that prints "Child" if age is less than 13
+Add an elif that prints "Teenager" if age is less than 18
+Add an else that prints "Adult"'''
+
+# Create age variable
+age = 25
+# Write if/elif/else
+if age < 13:
+    print("Child")
+elif age <18:
+    print("Teenager")
+else:
+    print('Adult')
