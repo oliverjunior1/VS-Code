6166061904e79def731501ff11eeec386d6c7393
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:52:41 -0300

    Create 3_list_tuple_dict_set_array.py

---
+++ w3_treinamentos/Python/Python/treinamentos/3_list_tuple_dict_set_array.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+a = [1,2,3]
+print(type(a))
+
+b = (1,2,3)
+print(type(b))
+
+c = {'a':1, 'b':2, 'c':3}
+print(type(c))
+
+d = {1,2,3}
+print(type(d))
+
+e = pd.array([1,5,7])
+print(type(e))
