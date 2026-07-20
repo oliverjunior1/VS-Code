e81036725d7f835f9a254ecaab73443f1def2ceb
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:33 -0300

    Create 1_exercicio_variaveis.py

---
+++ w3_treinamentos/Python/Python/treinamentos/d_exercicios_práticos_1/1_exercicio_variaveis.py
@@ -0,0 +1,11 @@
+# Crie variáveis para guardar seu nome, idade, altura e se você estuda Python (True ou False).
+# Depois, exiba todas em um print.
+
+nome = "Joaquim"
+altura = 1.75
+estuda_python = True
+resposta = 'sim' if estuda_python==True else 'não'
+
+
+print(f"O nome é {nome}, a altura é {altura} e ele estuda python: {resposta}."
+      )
