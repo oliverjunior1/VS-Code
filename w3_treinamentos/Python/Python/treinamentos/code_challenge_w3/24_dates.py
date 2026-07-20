21eca066272a25867624e38028fab21a57313ef9
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:14 -0300

    Create 24_dates.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/24_dates.py
@@ -0,0 +1,10 @@
+'''Import the datetime module
+Store the current date and time in a variable called x
+Print the year from x'''
+
+# Import the datetime module
+import datetime
+# Store the current date and time in x
+x = datetime.datetime.now()
+# Print the year
+print(x.year)
