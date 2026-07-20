c20a61a9eadd887e0b2aaf1a4885094c8e9efd1a
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:18 -0300

    Create 3_comments_challenge.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/3_comments_challenge.py
@@ -0,0 +1,15 @@
+# Write a single-line comment
+# This is a comment
+# Comment out this line so it does not run:
+#print("This should not run")# This should not run
+
+# Add a multiline comment
+'''
+When I think about Jesus,
+I think how much I have to change,
+because I am a sinner,
+And Jesus is a cleaner.
+'''
+
+
+
