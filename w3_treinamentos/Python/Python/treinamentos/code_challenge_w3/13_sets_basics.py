eebfea15bcea281b5235937773cc8c112fc7941c
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:53:44 -0300

    Create 13_sets_basics.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/13_sets_basics.py
@@ -0,0 +1,16 @@
+'''Create a set called colors with the values "red", "green", "blue"
+Print the set
+Add "yellow" to the set using add()
+Remove "green" from the set using discard()
+Print the number of items using len()'''
+
+# Create the set
+colors = {"red", "green", "blue"}
+# Print the set
+print(colors)
+# Add "yellow"
+colors.add("yellow")
+# Remove "green"
+colors.discard("green")
+# Print the number of items
+print(len(colors))
