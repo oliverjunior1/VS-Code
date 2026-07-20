1c7f64be7d398ec100b4fece4442a0b0b531815e
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:53:53 -0300

    Create 17_while.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/17_while.py
@@ -0,0 +1,15 @@
+'''Create a variable i with the value 0
+Write a while loop that runs as long as i is less than 6
+Inside the loop: increment i by 1
+If i equals 3, use continue to skip that iteration
+Print i'''
+
+# Create the i variable
+i = 0
+# While loop: print 1-5, skip 3 with continue
+while i <6:
+    i += 1
+    if i==3:
+        continue
+    print(i)
+
