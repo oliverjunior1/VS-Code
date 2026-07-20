0e22be99f2a72840d2435dcc0006e3cb01565459
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:12 -0300

    Create 23_module.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/23_module.py
@@ -0,0 +1,7 @@
+'''Import the platform module
+Print the system platform using platform.system()'''
+
+# Import the platform module
+import platform
+# Print the system platform
+print(platform.system())
