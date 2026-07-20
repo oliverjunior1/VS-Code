8680ee9cc187424372bd19435d389b9e48d59405
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-20 20:54:08 -0300

    Create 21_arrays.py

---
+++ w3_treinamentos/Python/Python/treinamentos/code_challenge_w3/21_arrays.py
@@ -0,0 +1,13 @@
+'''Create a list called cars with the values "Ford", "Volvo", "BMW"
+Print the first item in the list
+Change the second item to "Toyota"
+Print the list'''
+
+# Create a list
+cars = ["Ford", "Volvo", "BMW"]
+# Print the first item
+print(cars[0])
+# Change the second item to "Toyota"
+cars[1] = "Toyota"
+# Print the list
+print(cars)
