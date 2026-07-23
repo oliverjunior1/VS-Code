a4dc3dbedc2869e2658a669fde8b470f7a8f043b
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-22 23:19:47 -0300

    treinamento

--- w3_treinamentos/Python/Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ w3_treinamentos/Python/Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -1,22 +1,34 @@
 import random
 def mega():
-    x = sorted(list(random.sample(1,61),6))
-    return x
+    x = sorted(list(random.sample(range(1,61),6)))
     print(x)
+    return x
+    
 
 def facil():
-    x = sorted(list(random.sample(1,26),15))
-    return x
+    x = sorted(list(random.sample(range(1,26),15)))
     print(x)
+    return x
+    
 
-def mega():
-    x = sorted(list(random.sample(1,37),7))
-    y = random.choice(['jan', 'feb','mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'out', 'nov', 'dec'])
-    return x,y
-    print(x, y)
+def luckday():
+    x = sorted(list(random.sample(range(1,37),7)))
+    month = random.choice(["jan", 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'out', 'nov', 'dec'])
+    print(x, month)
+    return x, month
+    
 
 while True:
-    
+    option = int(input("Select 1 to megasena, 2 to lotofacil, 3 to luckday and 4 to exit: "))
+    match option:
+        case 1:
+            mega()
+        case 2:
+            facil()
+        case 3:
+            luckday()
+        case 4:
+            break
 
 
 
