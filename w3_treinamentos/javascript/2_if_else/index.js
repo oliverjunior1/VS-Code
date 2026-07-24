570ed2f53702050e90f1d7963d6cbdf37840250a
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-23 23:15:51 -0300

    Update index.js

--- w3_treinamentos/javascript/2_if_else/index.js
+++ w3_treinamentos/javascript/2_if_else/index.js
@@ -35,15 +35,22 @@
 // }
 // alert(greeting)
 
-let time = 10;
-if (time < 10) {
-    greeting = "Good morning";
-}
-else if (time < 20) {
-    greeting = "Good day";
-}
-else {
-    greeting = "Good evening";
-}
+// let time = 10;
+// if (time < 10) {
+//     greeting = "Good morning";
+// }
+// else if (time < 20) {
+//     greeting = "Good day";
+// }
+// else {
+//     greeting = "Good evening";
+// }
+// alert(greeting)
 
-alert(greeting)
+let text;
+if (Math.random() < 0.5) {
+    text = "<a href='https://w3schools.com'>Visit W3Schools</a>";
+} else {
+    text = "<a href='https://wwf.org'>Visit WWF</a>";
+}
+document.getElementById('demo').innerHTML = text;
