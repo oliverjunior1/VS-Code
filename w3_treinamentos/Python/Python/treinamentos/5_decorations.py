9774676fb4c7099429780bf5761b559680fade11
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-26 19:05:36 -0300

    treinamento

---
+++ w3_treinamentos/Python/Python/treinamentos/5_decorations.py
@@ -0,0 +1,15 @@
+def fun1(x):
+    def fun2():
+        print("#########################")
+        x()
+        print("#########################")
+    return fun2
+
+@fun1
+def greetings():
+    print("Hello Joaquim!!!")
+
+greetings()
+greetings()
+greetings()
+
