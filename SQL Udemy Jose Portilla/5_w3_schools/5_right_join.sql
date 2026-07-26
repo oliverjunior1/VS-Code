a43c9b809369cb660460146bccd9dec2889214f4
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-26 12:12:32 -0300

    Update 5_right_join.sql

--- SQL Udemy Jose Portilla/5_w3_schools/5_right_join.sql
+++ SQL Udemy Jose Portilla/5_w3_schools/5_right_join.sql
@@ -1,4 +1,6 @@
-SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
-FROM Orders
-RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
-ORDER BY Orders.OrderID;
+-- SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+-- FROM Orders
+-- RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+-- ORDER BY Orders.OrderID;
+
+
