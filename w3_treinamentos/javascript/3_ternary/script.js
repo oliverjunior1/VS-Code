0c611c4d64597e2bc58dfa4c18b2ca413ef5780f
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-26 22:29:22 -0300

    ternary javascript

--- w3_treinamentos/javascript/3_ternary/script.js
+++ w3_treinamentos/javascript/3_ternary/script.js
@@ -2,8 +2,9 @@
 // let text = (age< 18) ? "Minor":"Adult";
 // alert(text)
 
-let isMember = true;
-let value = 3000;
-let discount = isMember?0.2:0;
-let valueToPay =(1-discount)*value;
-alert(valueToPay);
+// let isMember = true;
+// let value = 3000;
+// let discount = isMember?0.2:0;
+// let valueToPay =(1-discount)*value;
+// alert(valueToPay);
+
