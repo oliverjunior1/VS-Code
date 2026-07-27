37a53aff86aba207041e2abad6e57cf802d9ff05
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-27 20:01:22 -0300

    Csharp user input

--- w3_treinamentos/CSharp/Program.cs
+++ w3_treinamentos/CSharp/Program.cs
@@ -112,17 +112,3 @@
 //     }
 // }
 
-using System;
-
-namespace Program
-{
-    class UserInput
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Put your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is: " + age);
-        }
-    }
-}
--- w3_treinamentos/CSharp/obj/Debug/net10.0/CSharp.AssemblyInfo.cs
+++ w3_treinamentos/CSharp/obj/Debug/net10.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a80c5a48b54b29c857afe107e1097bb7d4e7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec48671f0f7cded914994e6cf4e15e71ee8705b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
