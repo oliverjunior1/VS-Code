8ce92c70e0bd9c72890846e5173b9ff1dfe56693
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-27 19:55:50 -0300

    Csharp user input

--- w3_treinamentos/CSharp/Program.cs
+++ w3_treinamentos/CSharp/Program.cs
@@ -112,18 +112,3 @@
 //     }
 // }
 
-using System;
-using System.ComponentModel;
-
-namespace Program
-{
-    class UserInput
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Put your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is: " + age);
-        }
-    }
-}
