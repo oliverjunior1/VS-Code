c688633cd53ab668bcdb26b9df80e9379bcde131
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-27 21:58:45 -0300

    treinamento

--- w3_treinamentos/php/4_data_types.php
+++ w3_treinamentos/php/4_data_types.php
@@ -15,6 +15,21 @@
 // $x = true;
 // var_dump($x);
 
-$cars = array("Volvo", "BMW", "Toyota");
-var_dump($cars);
+// $cars = array("Volvo", "BMW", "Toyota");
+// var_dump($cars);
+
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+  public function message() {
+    return "My car is a " . $this->color . " " . $this->model . "!";
+  }
+}
+
+$myCar = new Car("red", "Volvo");
+var_dump($myCar);
 ?>
