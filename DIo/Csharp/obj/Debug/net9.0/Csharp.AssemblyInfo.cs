29370f6e80029b556127250013a86b9cc2cf3436
oliverjunior1 <oliver-junior@hotmail.com>
2026-07-31 19:36:51 -0300

    Update Csharp.AssemblyInfo.cs

--- DIo/Csharp/obj/Debug/net9.0/Csharp.AssemblyInfo.cs
+++ DIo/Csharp/obj/Debug/net9.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ad485afd6b37e978e716109ecf6f3eee9102a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bc5a34cd68a35c8c2024da4e6cb2683b1b3e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
