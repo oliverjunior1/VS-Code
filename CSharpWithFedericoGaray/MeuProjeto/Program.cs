bc7de6b2a34642e140facaacab86bcd24116a605
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-03 18:19:25 -0300

    CSharp casting

--- CSharpWithFedericoGaray/MeuProjeto/Program.cs
+++ CSharpWithFedericoGaray/MeuProjeto/Program.cs
@@ -1,24 +1,26 @@
-﻿using System;
+﻿// using System;
 
-namespace Program
-{
-    class Casting
-    {
-        static void Main(string[] args)
-        {   // casting
-            // Conversión Explícita
-            double miDouble = 12.37;
-            int miInt;
-            miInt = (int)miDouble;
-            Console.WriteLine(miInt);
-            // Conversión implícita
-            int num = 123456;
-            long numg = num;
-            Console.WriteLine(numg);
-            string miString = miDouble.ToString();
-            Console.WriteLine(miString);
+// namespace Program
+// {
+//     class Casting
+//     {
+//         static void Main(string[] args)
+//         {   // casting
+//             // Conversión Explícita
+//             double miDouble = 12.37;
+//             int miInt;
+//             miInt = (int)miDouble;
+//             Console.WriteLine(miInt);
+//             // Conversión implícita
+//             int num = 123456;
+//             long numg = num;
+//             Console.WriteLine(numg);
+//             string miString = miDouble.ToString();
+//             Console.WriteLine(miString);
 
 
-        }
-    }
-}
+//         }
+//     }
+// }
+
+
