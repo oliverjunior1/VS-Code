ac5a8c548f43814fe282e028ca8269507415f706
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-03 18:17:15 -0300

    CSharp casting

--- CSharpWithFedericoGaray/MeuProjeto/Program.cs
+++ CSharpWithFedericoGaray/MeuProjeto/Program.cs
@@ -6,10 +6,18 @@ namespace Program
     {
         static void Main(string[] args)
         {   // casting
+            // Conversión Explícita
             double miDouble = 12.37;
             int miInt;
             miInt = (int)miDouble;
             Console.WriteLine(miInt);
+            // Conversión implícita
+            int num = 123456;
+            long numg = num;
+            Console.WriteLine(numg);
+            string miString = miDouble.ToString();
+            Console.WriteLine(miString);
+
 
         }
     }
--- CSharpWithFedericoGaray/MeuProjeto/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
+++ CSharpWithFedericoGaray/MeuProjeto/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53818d2a374157580ace540e994a3f58e7dc3724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbf1d8f323486aac72dfd14640ccd6933150e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
