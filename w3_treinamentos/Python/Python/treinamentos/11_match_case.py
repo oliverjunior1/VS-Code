ca71d9df6aa8fbf7de6b04577a82c7f8068876bd
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-10 15:36:35 -0300

    Python Match

--- w3_treinamentos/Python/Python/treinamentos/11_match_case.py
+++ w3_treinamentos/Python/Python/treinamentos/11_match_case.py
@@ -1,2 +1,30 @@
 while True:
-    day = int(input("Put a number to see the day"))
+    day = int(input("Put a number to see the day, and 8 to exit: "))
+    match day:
+        case 1:
+            print("Sunday")
+        
+        case 2:
+            print("Monday")
+        
+        case 3:
+            print("Tuesday")
+        
+        case 4:
+            print("Wednesday")
+        
+        case 5:
+            print("Thursday")
+        
+        case 6:
+            print("Friday")
+        
+        case 7:
+            print("Saturday")
+        
+        case 8:
+            break
+        
+        case _:
+            print("Invalid number!")
+        
