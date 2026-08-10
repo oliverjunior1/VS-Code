987da46774b0beb4587d281b9d7e92c35772cc9d
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-09 22:22:46 -0300

    Python decorations

---
+++ w3_treinamentos/Python/Python/treinamentos/11_decorations_with_arguments.py
@@ -0,0 +1,16 @@
+def fun_1(x):
+    def fun_2(*args):
+        print("###########################")
+        x(*args)
+        print("###########################")
+    return fun_2
+
+@fun_1
+def greetings(*args):
+    for arg in args:
+        print(f"Hello {arg}")
+
+greetings("Mariane", "Joao")
+greetings("Mariane", "Joao")
+greetings("Mariane", "Joao")
+greetings("Mariane", "Joao")
