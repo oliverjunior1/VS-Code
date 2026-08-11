306859b5abfd0acaa6e0f7b1bb2821e8219deba1
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-10 21:35:57 -0300

    Python functions

---
+++ Python_Phil_E_and_Federico/5_exercicios_funcoes.py
@@ -0,0 +1,8 @@
+"""Write a function that requires an indefinite number of arguments. What this function must do is return 
+True if at any time the number zero has been entered twice consecutively.
+For example:
+(5,6,1,0,0,9,3,5) >>>
+True
+(6,0,5,1,0,3,0,1) >>>
+False"""
+
