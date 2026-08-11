96a6f1386ac4058f6efd79f86c3e4a14fe0da150
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-10 21:41:02 -0300

    Python functions

--- Python_Phil_E_and_Federico/5_exercicios_funcoes.py
+++ Python_Phil_E_and_Federico/5_exercicios_funcoes.py
@@ -6,3 +6,20 @@ True
 (6,0,5,1,0,3,0,1) >>>
 False"""
 
+def neighboring_zeros(*args):
+
+    counter = 0
+
+    for n in args:
+
+        if counter + 1 == len(args):
+            return False
+        elif args[counter] == 0 and args[counter + 1] == 0:
+            return True
+        else:
+            counter += 1
+
+    return False
+
+
+print(neighboring_zeros(0, 4, 0, 8, 1, 3, 9, 8, 0, 2, 0))
