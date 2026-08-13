f5ef7286eca0faf58921edd33d893bf727ff9144
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-12 21:14:44 -0300

    Python training

--- w3_treinamentos/Python/Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ w3_treinamentos/Python/Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -1,32 +1,28 @@
 import random
+
 def mega():
-    x = sorted(list(random.sample(range(1,61),6)))
+    x = list(sorted(random.sample(range(1,61),6)))
     print(x)
-    return x
-    
 
 def facil():
-    x = sorted(list(random.sample(range(1,26),15)))
+    x = list(sorted(random.sample(range(1,26),15)))
     print(x)
-    return x
-    
 
-def luckday():
-    x = sorted(list(random.sample(range(1,37),7)))
-    month = random.choice(["jan", 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'out', 'nov', 'dec'])
-    print(x, month)
-    return x, month
-    
+def luck_day():
+    month = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'out', 'nov', 'dec']
+    choiced = random.choice(month)
+    x = list(sorted(random.sample(range(1,35),7)))
+    print(choiced, x)
 
 while True:
-    option = int(input("Select 1 to megasena, 2 to lotofacil, 3 to luckday and 4 to exit: "))
+    option = int(input("Put 1 to megasena, 2 to lotofacil, 3 to luckday or 4 to exit: "))
     match option:
         case 1:
             mega()
         case 2:
             facil()
         case 3:
-            luckday()
+            luck_day()
         case 4:
             break
 
