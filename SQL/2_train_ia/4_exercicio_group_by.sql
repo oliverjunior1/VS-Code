c82218284b79196155613b54632d67c4ac0fcb65
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-14 19:31:20 -0300

    Create 4_exercicio_group_by.sql

---
+++ SQL/2_train_ia/4_exercicio_group_by.sql
@@ -0,0 +1,59 @@
+/*CREATE TABLE funcionarios (
+    departamento VARCHAR(50),
+    salario DECIMAL(10,2)
+);*/
+/*
+INSERT INTO funcionarios (departamento, salario)
+VALUES
+('TI', 5000),
+('TI', 7000),
+('RH', 4000),
+('RH', 6000),
+('Financeiro', 8000);*/
+
+--1. Média salarial por departamento.
+--SELECT departamento, AVG(salario) FROM funcionarios GROUP BY departamento;
+
+--2. Total salarial por departamento.
+--SELECT departamento,SUM(salario) FROM funcionarios GROUP BY departamento;
+
+--3. Departamentos com média acima de 5500.
+/*SELECT departamento,
+       AVG(salario) AS media_salarial
+FROM funcionarios
+GROUP BY departamento
+HAVING AVG(salario) > 5500;*/
+
+
+/*CREATE TABLE clientes_vendas (
+    cliente VARCHAR(50),
+    valor DECIMAL(10,2)
+);*/
+
+/*INSERT INTO clientes_vendas (cliente, valor)
+VALUES
+('Ana', 100),
+('Ana', 200),
+('Ana', 150),
+('Pedro', 300),
+('Pedro', 100),
+('Julia', 500);*/
+
+--1. Total gasto por cliente.
+SELECT cliente, SUM(valor) AS soma_cliente FROM clientes_vendas  GROUP BY cliente;
+
+--2. Quantidade de pedidos.
+SELECT cliente, COUNT(cliente) AS quantidade_de_compras FROM clientes_vendas GROUP BY cliente;
+
+--3. Ticket médio.
+SELECT cliente, SUM(valor)/COUNT(cliente) AS ticket_por_cliente FROM clientes_vendas GROUP BY cliente;
+
+--4. Clientes que gastaram mais de 300.
+-- Minha solução
+SELECT cliente, SUM(valor) AS total_compras, SUM(valor)>300 AS gastou_mais_que_300 FROM clientes_vendas GROUP BY cliente;
+-- solução chatgpt
+SELECT cliente,
+       SUM(valor) AS total_compras
+FROM clientes_vendas
+GROUP BY cliente
+HAVING SUM(valor) > 300;
