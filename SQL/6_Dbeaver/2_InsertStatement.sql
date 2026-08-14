55b7f3857f4984e83f17749697669d2853b6fb72
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-14 19:55:00 -0300

    SQL Insert

--- SQL/6_Dbeaver/2_InsertStatement.sql
+++ SQL/6_Dbeaver/2_InsertStatement.sql
@@ -1,6 +1,7 @@
 SELECT * FROM Artist;
 
-SELECT * FROM Artist WHERE Name = "Nicki Minaj";
+SELECT * FROM Artist WHERE Name = "Nicki Minaj"; --- Dont appear nothing
 
 INSERT INTO Artist (ArtistId, Name) VALUES (276, "Nicki Minaj");
 
+SELECT * FROM Artist WHERE Name = "Nicki Minaj"; --- Appear her name now
