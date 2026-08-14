adcce84f4a0adaf30f839e072573d9da878628ed
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-14 19:32:23 -0300

    Create 4_left_join.sql

---
+++ SQL/5_w3_schools/4_left_join.sql
@@ -0,0 +1,11 @@
+-- SELECT Customers.CustomerName, Orders.OrderID
+-- FROM Customers
+-- LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+-- ORDER BY Customers.CustomerName;
+
+-- SELECT Customers.CustomerName, Orders.OrderID
+-- FROM Customers
+-- LEFT JOIN Orders
+-- ON Customers.CustomerID = Orders.CustomerID
+-- WHERE Orders.CustomerID IS NULL;
+
