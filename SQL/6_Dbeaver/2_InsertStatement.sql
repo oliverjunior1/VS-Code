acd12858923b88603321127d0e71d0f2610a4e63
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-14 20:01:01 -0300

    SQL Insert

--- SQL/6_Dbeaver/2_InsertStatement.sql
+++ SQL/6_Dbeaver/2_InsertStatement.sql
@@ -10,6 +10,8 @@ SELECT * FROM Artist WHERE Name = 'Cardi B'; --- There's no record
 
 INSERT INTO Artist VALUES ('Cardi B', 277); --- Here give an error
 
-INSERT INTO Artist VALUES (277, 'Cardi B'); --- Now it's work
+INSERT INTO Artist VALUES (277, 'Cardi B'); --- Now it's work, or we need to put it in order!
+
+
 
 
