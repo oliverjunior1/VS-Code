475969c30fbc4b62b694e6db78b551ec73728c6b
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-15 19:18:27 -0300

    Python trainer

--- Treinamentos/Python/Python/treinamentos/5_decorations.py
+++ Treinamentos/Python/Python/treinamentos/5_decorations.py
@@ -1,15 +1,2 @@
-def fun1(x):
-    def fun2():
-        print("#########################")
-        x()
-        print("#########################")
-    return fun2
 
-@fun1
-def greetings():
-    print("Hello Joaquim!!!")
-
-greetings()
-greetings()
-greetings()
 
