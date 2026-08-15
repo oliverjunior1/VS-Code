3d70421a263a15029338a46a72a351572237aeea
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-15 19:22:34 -0300

    Python trainer

--- Treinamentos/Python/Python/treinamentos/5_decorations.py
+++ Treinamentos/Python/Python/treinamentos/5_decorations.py
@@ -1,2 +1,13 @@
+def fun1(x):
+    def fun2():
+        print("#############################")
+        x()
+        print("#############################")
+    return fun2
 
+@fun1
+def greetings():
+    print("Jesus is the light of the world!")
+
+greetings()
 
