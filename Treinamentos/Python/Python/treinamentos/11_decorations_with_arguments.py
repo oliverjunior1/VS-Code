84eb8ac3a0f08b4fe84e8b7db585adfd212ef992
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-16 11:36:28 -0300

    Python trainer

--- Treinamentos/Python/Python/treinamentos/11_decorations_with_arguments.py
+++ Treinamentos/Python/Python/treinamentos/11_decorations_with_arguments.py
@@ -1,14 +1,1 @@
-def fun1(x):
-    def fun2(*args):
-        print('########################')
-        x(*args)
-        print('########################')
-    return fun2
 
-@fun1
-def greetings(*args):
-    for arg in args:
-        print(f"Hello {arg}")
-    
-
-greetings("Joaquim", "Alyne")
