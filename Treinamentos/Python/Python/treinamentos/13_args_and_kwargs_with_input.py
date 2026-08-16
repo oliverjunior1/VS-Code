5c7f94419e996a0fa2bdf98db7ce60198095e864
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-16 12:20:48 -0300

    Python trainer

--- Treinamentos/Python/Python/treinamentos/13_args_and_kwargs_with_input.py
+++ Treinamentos/Python/Python/treinamentos/13_args_and_kwargs_with_input.py
@@ -2,13 +2,13 @@ def mostrar_dados(*args, **kwargs):
     for num in args:
         print(num)
 
-    print('\n Informações exttras')
+    print('\n Informações extras')
 
     for chave, valor in kwargs.items():
-        print(f"{chave}:{valor}")
+        print(f'{chave}:{valor}')
 
 
-#======================Recebendo_valores==============================
+#=======================Recebendo_valores===============================
 
 numeros = input('Digite números separados por espaços: ')
 numeros = tuple(map(int, numeros.split()))
@@ -16,4 +16,4 @@ numeros = tuple(map(int, numeros.split()))
 nome = input("Digite seu nome: ")
 cidade = input("Digite sua cidade: ")
 
-mostrar_dados(*numeros,nome = nome, cidade=cidade)
+mostrar_dados(*numeros, nome = nome, cidade=cidade)
