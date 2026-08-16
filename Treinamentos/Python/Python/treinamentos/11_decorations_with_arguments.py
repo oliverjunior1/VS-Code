42b60d0db00eb4dcde1a67e5dfa381a760933984
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-16 11:43:53 -0300

    Python trainer

--- Treinamentos/Python/Python/treinamentos/11_decorations_with_arguments.py
+++ Treinamentos/Python/Python/treinamentos/11_decorations_with_arguments.py
@@ -1,1 +1,14 @@
+def fun1(x):
+    def fun2(*args):
+        print("########################")
+        x(*args)
+        print("########################")
+    return fun2
 
+@fun1
+def greetings(*args):
+    for arg in args:
+        print(f"Bless you are {arg}, in the name of Jesus!")
+
+
+greetings("Joaquim", "Alyne", "Mariane", "Pepe")
