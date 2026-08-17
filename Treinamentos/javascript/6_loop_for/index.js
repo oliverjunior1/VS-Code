d5e439686d92a0bb015bbccb8d04de197a305ac4
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-17 19:13:37 -0300

    open and manipulate dates with python

--- Treinamentos/javascript/6_loop_for/index.js
+++ Treinamentos/javascript/6_loop_for/index.js
@@ -3,3 +3,12 @@
 //     console.log(i)
 // }
 
+// const cars = ['BMW', "Volvo", "Saab", "Ford"]
+// let len = cars.length
+
+// let text = ""
+// for (let i = 0; i<len; i++) {
+//     text += cars[i] + " "
+//     console.log(text)
+// }
+
