ed4af4779b669ccee98a1aa6199a6487e4f7acba
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-17 19:31:57 -0300

    open and manipulate dates with python

---
+++ Python_Phil_E_and_Federico/8_open_and_manipulate_dates.py
@@ -0,0 +1,8 @@
+"""
+Open and Manipulate Files Practice #1
+Open the file my_text.txt and print its content.
+
+Note: assume that the file is saved in the same folder where your code is located"""
+
+f = open("my_text.txt")
+print(f.read())
