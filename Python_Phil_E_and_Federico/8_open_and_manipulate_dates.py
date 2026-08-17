084c05e5b5c7f1ae1998b5acdd05583353ba8ec0
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-17 19:44:30 -0300

    open and manipulate dates with python

--- Python_Phil_E_and_Federico/8_open_and_manipulate_dates.py
+++ Python_Phil_E_and_Federico/8_open_and_manipulate_dates.py
@@ -4,5 +4,9 @@ Open the file my_text.txt and print its content.
 
 Note: assume that the file is saved in the same folder where your code is located"""
 
-f = open("my_text.txt")
-print(f.read())
+# x = open("C:\\Users\\Olive\\VS Code\\Python_Phil_E_and_Federico\\test.txt", 'r')
+
+# print(x.readlines())
+
+with open("Python_Phil_E_and_Federico\\my_text.txt", 'r') as f:
+    print(f.readlines())
