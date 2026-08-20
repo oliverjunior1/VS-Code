05dfc16b26ca7dd95a4a88217c3457f847a3c32f
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-20 20:10:03 -0300

    Project receipt python

--- Python_Phil_E_and_Federico/9_project_receipt.py
+++ Python_Phil_E_and_Federico/9_project_receipt.py
@@ -1,2 +1,51 @@
 from pathlib import Path
 
+while True:
+    option_country = int(input("Type 1 to brazilian food, 2 to italian food, 3 to spanish food and 4 to exit: "))
+    match option_country:
+        case 1:
+            option_food = int(input(f"Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
+            match option_food:
+                case 1:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Feijoada.txt")
+                    print(x)
+                case 2:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Moqueca capixaba.txt")
+                    print(x)
+                case 3:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Pão de queijo.txt")
+                case 4:
+                    break
+
+        
+        case 2:
+            option_food = int(input(f"Put 1 lasagna, 2 to risotto, 3 to Tiramisu, and 4 to exit: "))
+            match option_food:
+                case 1:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Italiana\\LASAGNA ALLA BOLOGNESE.txt")
+                    print(x)
+                case 2:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Italiana\\RISOTTO ALLA MILANESE.txt")
+                    print(x)
+                case 3:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Italiana\\TIRAMISU.txt")
+                case 4:
+                    break
+
+        
+        case 3:
+            option_food = int(input(f"Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
+            match option_food:
+                case 1:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Feijoada.txt")
+                    print(x)
+                case 2:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Moqueca capixaba.txt")
+                    print(x)
+                case 3:
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Pão de queijo.txt")
+
+        case 4:
+            break
+
+        
