d957d45f83b9159386540c7fbdd5f867e79082fd
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-20 20:13:57 -0300

    Project receipt python

--- Python_Phil_E_and_Federico/9_project_receipt.py
+++ Python_Phil_E_and_Federico/9_project_receipt.py
@@ -4,7 +4,7 @@ while True:
     option_country = int(input("Type 1 to brazilian food, 2 to italian food, 3 to spanish food and 4 to exit: "))
     match option_country:
         case 1:
-            option_food = int(input(f"Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
+            option_food = int(input("Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
             match option_food:
                 case 1:
                     x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Feijoada.txt")
@@ -19,7 +19,7 @@ while True:
 
         
         case 2:
-            option_food = int(input(f"Put 1 lasagna, 2 to risotto, 3 to Tiramisu, and 4 to exit: "))
+            option_food = int(input("Put 1 lasagna, 2 to risotto, 3 to Tiramisu, and 4 to exit: "))
             match option_food:
                 case 1:
                     x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Italiana\\LASAGNA ALLA BOLOGNESE.txt")
@@ -34,16 +34,18 @@ while True:
 
         
         case 3:
-            option_food = int(input(f"Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
+            option_food = int(input("Put 1 to feijoada, 2 to moqueca capixaba and 3 to pão de queijo: "))
             match option_food:
                 case 1:
-                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Feijoada.txt")
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Espanhola\\CHURROS COM CHOCOLATE.txt")
                     print(x)
                 case 2:
-                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Moqueca capixaba.txt")
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Espanhola\\PAELLA VALENCIANA.txt")
                     print(x)
                 case 3:
-                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\\Comida Brasileira\\Pão de queijo.txt")
+                    x = open("C:\\Users\\Olive\\OneDrive\\Área de Trabalho\\Receita\Comida Espanhola\\TORTILLA ESPAÑOLA.txt")
+                case 4:
+                    break
 
         case 4:
             break
