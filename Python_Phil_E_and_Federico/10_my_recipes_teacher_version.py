8fde31dd31493c3c553098da49b3cf643077b98d
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-21 21:07:52 -0300

    my receipes - teacher version - python

---
+++ Python_Phil_E_and_Federico/10_my_recipes_teacher_version.py
@@ -0,0 +1,44 @@
+#show start menu
+
+menu = 0
+
+if menu == 1:
+    # show categories
+    # choose category
+    # show recipes
+    # choose recipe
+    # read recipe
+    # go back to menu
+    pass
+
+elif menu == 2:
+    # shows categorires
+    # choose category
+    # cerate new recipe
+    # go back to menu
+    pass
+
+elif menu == 3:
+    # create category
+    # go back to menu
+    pass
+
+elif menu == 4:
+    # show categories
+    # choose category
+    # show recipes
+    # choose recipe
+    # eliminate recipe
+    # go back to menu
+    pass
+
+elif menu == 5:
+    # show categories
+    # choose category
+    # eliminate category
+    # go back to menu
+    pass
+
+elif menu == 6:
+    # end program
+    pass
