e70b2be8523f8710446fdfa11821560ddd515f4f
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-24 19:27:42 -0300

    OOP - classes - Python

---
+++ Python_Phil_E_and_Federico/2_class/8_methods.py
@@ -0,0 +1,13 @@
+'''Create a class called Wizard, and create a method called cast_spell() (it should print "Abracadabra!").
+
+Create an instance of Wizard on the merlin object, and have it cast a spell.'''
+
+class Wizard:
+    def cast_spell():
+        print("Abracadabra!")
+
+merlin = Wizard()
+
+Wizard.cast_spell()
+
+
