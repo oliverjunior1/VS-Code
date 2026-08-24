e2bab7ace4d7151423bdaeaaabf22be5857efc2b
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-24 19:23:03 -0300

    OOP - classes - Python

---
+++ Python_Phil_E_and_Federico/2_class/7_methods.py
@@ -0,0 +1,8 @@
+'''Create a Dog class. Said dog must be able to bark.
+
+Create the bark() method and call it on an instance of Dog. Every time he barks, "Woof!" should be displayed on the screen.'''
+
+class Dog:
+    def bark():
+        print("Woof!")
+
