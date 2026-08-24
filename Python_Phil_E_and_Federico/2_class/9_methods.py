bf0e407020b7dbd59cab1bd1473784de7801867a
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-24 19:31:46 -0300

    OOP - classes - Python

---
+++ Python_Phil_E_and_Federico/2_class/9_methods.py
@@ -0,0 +1,8 @@
+'''Create an instance of the Alarm class, which has a method called snooze(minutes). The method should print the following message to the screen:
+
+"The alarm has been postponed {amount_minutes} minutes"'''
+
+class Alarm:
+    def snooze(minutes):
+        amount_minutes = sum(minutes)
+        print(f"The alarm has been postponed {amount_minutes} minutes")
