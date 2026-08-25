dba549a5de5718e9075eb3be4d84832e0fff98d3
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-25 20:41:25 -0300

    types of methods - Python

---
+++ Python_Phil_E_and_Federico/2_class/16_types_of_methods.py
@@ -0,0 +1,4 @@
+'''Create an instance method throw_arrow() that subtracts by -1 the number of arrows a Character 
+instance has, which in turn has an instance attribute called arrows_amount (that stores a certain number).'''
+
+
