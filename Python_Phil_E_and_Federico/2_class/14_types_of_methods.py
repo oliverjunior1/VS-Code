a74210c0fc8a8986b92a52b0091d5ada00f8f291
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-25 20:24:40 -0300

    types of methods - Python

--- Python_Phil_E_and_Federico/2_class/14_types_of_methods.py
+++ Python_Phil_E_and_Federico/2_class/14_types_of_methods.py
@@ -1,3 +1,12 @@
 '''Create a static method: breathe() for the Pet class. When called, 
 it should print to the screen "Inhale... Exhale"'''
 
+class Pet:
+    @staticmethod
+    def breathe():
+        print("Inhale... Exhale")
+
+
+Dog = Pet()
+
+Dog.breathe()
