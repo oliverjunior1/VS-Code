db2db9819388127f8fa1ee892315e75689ce2ec1
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-26 19:51:54 -0300

    OOP - Python

---
+++ Python_Phil_E_and_Federico/OOP/4_extended_inheritance.py
@@ -0,0 +1,3 @@
+'''If the Daughter class has inherited her way of laughing from her father, and her vocation 
+from her mother, and today they have the same job at the Prosecutor's Office, create multiple 
+inheritance that allows this class to inherit correctly from Father and Mother.'''
