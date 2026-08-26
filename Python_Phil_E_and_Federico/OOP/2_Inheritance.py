703646de0cbfd5ea84581a86bff8c5e11fd6f273
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-26 19:25:40 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/2_Inheritance.py
+++ Python_Phil_E_and_Federico/OOP/2_Inheritance.py
@@ -1,4 +1,12 @@
 '''Create a class called Pet, which has the following instance attributes: age, name, legs. 
 Create another class, Dog, which inherits its attributes from the first.'''
 
+class Pet:
+    def __init__(self, age, name, legs):
+        self.age = age
+        self.name = name
+        self.legs = legs
 
+
+class Dog(Pet):
+    pass
