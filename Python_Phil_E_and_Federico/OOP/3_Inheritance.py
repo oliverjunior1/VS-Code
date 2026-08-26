133852c680f1ed20f6a257e84c0aaf310df0b833
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-26 19:26:47 -0300

    OOP - Python

---
+++ Python_Phil_E_and_Federico/OOP/3_Inheritance.py
@@ -0,0 +1,3 @@
+'''Create a class called Vehicle, which contains the speed() and slow_down() methods 
+(you can leave the method code blank with pass). Create a class called Car that inherits 
+these methods from Vehicle.'''
