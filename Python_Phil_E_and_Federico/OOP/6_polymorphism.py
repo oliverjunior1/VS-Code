618d3ee3f44bdb6e1529106b34766f332cb2beb3
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-27 18:56:56 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/6_polymorphism.py
+++ Python_Phil_E_and_Federico/OOP/6_polymorphism.py
@@ -3,3 +3,4 @@ Imagine uma classe Animal com um método falar().
 A classe Cachorro sobrescreve esse método para retornar "Au au".
 A classe Gato sobrescreve para retornar "Miau".
 Quando você chama falar() em cada objeto, o resultado muda conforme o tipo do animal.'''
+
