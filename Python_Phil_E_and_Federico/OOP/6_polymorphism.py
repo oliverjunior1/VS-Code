638415b727473f99208c41f87dec81d0b04912c4
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-27 18:47:09 -0300

    OOP - Python

---
+++ Python_Phil_E_and_Federico/OOP/6_polymorphism.py
@@ -0,0 +1,6 @@
+'''The built-in function in Python len() has a polymorphic behavior, since it calculates 
+the length of an object based on its type (strings, lists, tuples, among others), returning 
+the number of items or characters that make it.
+Create an iterator that iterates through the following objects: word, list, tuple and displays 
+on the screen (using print()) for each of them its length with the len() function.'''
+
