df122c10bb6beda6c153030e5da8ee1dd276a0c1
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-27 19:03:52 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/6_polymorphism.py
+++ Python_Phil_E_and_Federico/OOP/6_polymorphism.py
@@ -4,3 +4,20 @@ A classe Cachorro sobrescreve esse método para retornar "Au au".
 A classe Gato sobrescreve para retornar "Miau".
 Quando você chama falar() em cada objeto, o resultado muda conforme o tipo do animal.'''
 
+class Animal:
+    def falar():
+        print("O animal fala")
+
+
+class Cachorro(Animal):
+    def falar():
+        print("Au au")
+
+class Gato(Animal):
+    def falar():
+        print("Miau")
+
+
+Gato.falar()
+Cachorro.falar()
+Animal.falar()
