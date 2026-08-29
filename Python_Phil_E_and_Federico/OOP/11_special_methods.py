4be030cac6b68bf95d59f9752a5f0db00acf8a01
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-29 10:12:50 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/11_special_methods.py
+++ Python_Phil_E_and_Federico/OOP/11_special_methods.py
@@ -1,16 +1,17 @@
 '''Given the Book class, implement the special method __del__ so that the user is informed with the message 
 "Book deleted", showing it on the screen every time a book is deleted.'''
 
-class Book():
+class Book:
     def __init__(self, title, author, pages):
         self.title = title
         self.author = author
         self.pages = pages
-        
-    def __del__(self, instance):
-        return self.instance
 
-    
+    def __del__(self):
+        print("Book deleted")
 
-Adminstracao = Book("A Administracao segundo", "Agusto Cury", 212)
-print(Adminstracao.__delete__)
+# Criando um livro
+Administracao = Book("A Administração segundo", "Augusto Cury", 212)
+
+# Deletando o objeto
+del Administracao
