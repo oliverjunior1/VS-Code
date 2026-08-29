989f75f253cfec994b3a6d14f82d72a87fc0d3c2
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-29 10:06:40 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/11_special_methods.py
+++ Python_Phil_E_and_Federico/OOP/11_special_methods.py
@@ -1,19 +1,2 @@
-'''Given the Book class, implement the special method __len__ so that each time the len() 
-function is executed on it, it returns the number of pages as an integer.'''
-
-class Book:
-    def __init__(self, author, title, pages):
-        self.author = author
-        self.title = title
-        self.pages = pages
-
-    def __len__(self):
-        return self.pages
-
-    
-
-
-Acts = Book("Paul", "Acts",35)
-
-print(len(Acts))
-print(Acts)
+'''Given the Book class, implement the special method __del__ so that the user is informed with the message 
+"Book deleted", showing it on the screen every time a book is deleted.'''
