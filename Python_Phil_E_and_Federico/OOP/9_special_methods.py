e6af9e6ab683f323c33cdff22d26ec2838633816
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-28 21:04:12 -0300

    OOP - Python

--- Python_Phil_E_and_Federico/OOP/9_special_methods.py
+++ Python_Phil_E_and_Federico/OOP/9_special_methods.py
@@ -3,3 +3,14 @@ Special Methods Practice #1
 Given the Book class, implement the special method __str__ so that each time the object is printed, 
 it returns '"{title}", from {author}' (note: the title must be enclosed in double quotes).'''
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
+    def __str__(self):
+        return f"Title:{self.title}, \nAuthor:{self.author}"
+
+Plato = Book("Allegory of the cave", "Plato")
+
+print(Plato)
