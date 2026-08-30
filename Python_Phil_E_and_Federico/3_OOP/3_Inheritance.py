e02ae3ce284980dbf223b1b1c18400d5fa35d0ad
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-30 19:34:03 -0300

    Create 3_Inheritance.py

---
+++ Python_Phil_E_and_Federico/3_OOP/3_Inheritance.py
@@ -0,0 +1,13 @@
+'''Create a class called Vehicle, which contains the speed() and slow_down() methods 
+(you can leave the method code blank with pass). Create a class called Car that inherits 
+these methods from Vehicle.'''
+
+class Vehicle:
+    def speed():
+        pass
+
+    def slow_down():
+        pass
+
+class Car(Vehicle):
+    pass
