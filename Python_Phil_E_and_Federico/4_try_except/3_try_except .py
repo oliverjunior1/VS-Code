bd4eb7402c50b249c56aa073ffe0f14a123ebb0a
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-31 19:07:04 -0300

    try except - Python

--- Python_Phil_E_and_Federico/4_try_except/3_try_except .py
+++ Python_Phil_E_and_Federico/4_try_except/3_try_except .py
@@ -2,21 +2,12 @@
 I'll need you to do it in a slightly different way so that it can be tested: you'll need to implement it INSIDE the function. 
 In the form of a comment, you will see an example resolution. Keep in mind, however, that the preferred form is the one we've 
 seen in class.
-For the following division() function, implement an error handler:
-In the event of a type error (TypeError), the message should be printed on the screen: "Arguments must be numbers"
-If a division by zero is attempted (error of type ZeroDivisionError), the message displayed should be: "Second argument must
-not be zero"
-In the event that an error does not occur, it should limit itself to printing the result of the quotient (division) between the
-two numbers given as an argument.'''
-try:
-    x = int(input("Put the first number: "))
-    y = int(input("Put the second number: "))
-    division = x / y
-    print("Result:", division)
-except ValueError:
-    print("Arguments must be valid integers")
-except ZeroDivisionError:
-    print("Second argument must not be zero")
+Implement an error handler inside the following function, open_file():
+If the file you are trying to open cannot be found (FileNotFoundError), display the message: "The file was not found"
+In case another type of error occurs, display the message: "Unknown error"
+If no error occurs, print to screen: "Opening successfully"
+In all cases, at the end, print: "Ending execution"'''
+
 
 
 
