bf6e2a1838635348a6b9847a785e9d8c4ca9ffef
oliverjunior1 <oliver-junior@hotmail.com>
2026-08-31 19:23:16 -0300

    try except - Python

--- Python_Phil_E_and_Federico/4_try_except/3_try_except .py
+++ Python_Phil_E_and_Federico/4_try_except/3_try_except .py
@@ -7,11 +7,18 @@ If the file you are trying to open cannot be found (FileNotFoundError), display 
 In case another type of error occurs, display the message: "Unknown error"
 If no error occurs, print to screen: "Opening successfully"
 In all cases, at the end, print: "Ending execution"'''
+from pathlib import Path
 
 try:
-    pass
-except:
-    pass
+    file_path = Path("C:\\Users\\Olive\\VS Code\\Python_Phil_E_and_Federico\\4_try_except\\dados.txt")   # Create a Path object
+    open_file = file_path.open("r")   # Open the file in read mode
+    print("Opening successfully")
+    print(open_file.read())     # Read and print the file contents
+    open_file.close()
+except FileNotFoundError:
+    print("The file was not found")
+
+
 
 
 
