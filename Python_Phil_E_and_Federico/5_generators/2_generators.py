5a2c7ebc1ea722aad7b6cd99f1b8d850a3665ec8
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-01 19:16:22 -0300

    Generators - Python

---
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -0,0 +1,4 @@
+'''Create a generator (stored in the practice_generator variable) that is capable of returning an 
+infinite sequence of numbers, starting from 1, and returning a higher consecutive number each time 
+it is called using next.'''
+
