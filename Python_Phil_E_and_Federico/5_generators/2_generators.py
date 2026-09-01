70d3bf7b44226f722c80053099732a42f6d70f71
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-01 19:28:58 -0300

    Generators - Python

--- Python_Phil_E_and_Federico/5_generators/2_generators.py
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -2,3 +2,27 @@
 infinite sequence of numbers, starting from 1, and returning a higher consecutive number each time 
 it is called using next.'''
 
+def infinite_sequence():
+    num = 0
+    while True:
+        num += 1
+        yield num
+
+practice_generator = infinite_sequence()
+
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
