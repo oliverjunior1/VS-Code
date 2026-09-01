0eb2192d55be7af726d0b14d1e13895997221af2
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-01 19:33:44 -0300

    Generators - Python

--- Python_Phil_E_and_Federico/5_generators/2_generators.py
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -2,3 +2,32 @@
 infinite sequence of numbers, starting from 1, and returning a higher consecutive number each time 
 it is called using next.'''
 
+def generate_always():
+    num = 0
+    while True:
+        yield num
+        num += 1
+        
+
+x = generate_always()
+
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
