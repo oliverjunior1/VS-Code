0e7600b18dfc73215e7f3ecd28b33c8541a62a49
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 16:11:27 -0300

    Generators - Python

--- Python_Phil_E_and_Federico/5_generators/2_generators.py
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -1,3 +1,22 @@
 '''Create a generator (stored in the variable practice_generator) that is capable of returning 
 multiples of 7 indefinitely, starting from 7 itself, and that each time it is called returns the 
 next multiple (7, 14, 21, 28... ).'''
+
+
+def always_go():
+    num = 0
+    while True:
+        yield num
+        num += 7
+        
+
+practice_generator = always_go()
+
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+print(next(practice_generator))
+
+
