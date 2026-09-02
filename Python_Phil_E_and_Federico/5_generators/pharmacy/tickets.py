35cb12404faf212c8c2451d1044fd4c42584329e
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 19:36:02 -0300

    Update tickets.py

--- Python_Phil_E_and_Federico/5_generators/pharmacy/tickets.py
+++ Python_Phil_E_and_Federico/5_generators/pharmacy/tickets.py
@@ -1,29 +1,33 @@
 def perfume_tickets():
-    for n in range(1,10000):
-        yield f"P - {n}"
+    for n in range(1, 10000):
+        yield f'P - {n}'
+
 
 def medicine_tickets():
     for n in range(1, 10000):
-        yield f"M - {n}"
+        yield f'M - {n}'
+
 
 def cosmetic_tickets():
-    for n in range(1,10000):
-        yield f"C - {n}"
+    for n in range(1, 10000):
+        yield f'C - {n}'
+
 
 p = perfume_tickets()
 m = medicine_tickets()
 c = cosmetic_tickets()
 
+
 def decorator(product):
     print('\n' + '*' * 23)
-    print("Your number is:")
+    print('Your number is:')
 
-if product == 'P':
-    print(next(p))
-elif product == 'M':
-    print(next(m))
-else:
-    print(next(c))
+    if product == 'P':
+        print(next(p))
+    elif product == 'M':
+        print(next(m))
+    else:
+        print(next(c))
 
-print('Please wait for your turn')
-print('*' * 23 + '\n')
+    print('Please wait for your turn')
+    print('*' * 23 + '\n')
