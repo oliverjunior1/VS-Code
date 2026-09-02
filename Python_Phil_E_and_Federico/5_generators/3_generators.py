056cc9c049977d5bb217e0d7b4126d5c36f415d4
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 17:03:55 -0300

    Generators - Python

--- Python_Phil_E_and_Federico/5_generators/3_generators.py
+++ Python_Phil_E_and_Federico/5_generators/3_generators.py
@@ -6,3 +6,22 @@ message each time it is called:
 "Game Over"
 Store the generator in the variable lose_live'''
 
+def character():
+    lives = 4
+    while lives > 0:
+        yield lives
+        lives -= 1
+
+# Criar o generator
+lose_live = character()
+
+# Consumir os valores com next()
+try:
+    while True:
+        lives_left = next(lose_live)
+        print(f"You have {lives_left} lives left")
+except StopIteration:
+    print("Game Over")
+
+
+ 
