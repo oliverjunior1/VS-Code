4353c4f336e3007a114b9366ea0869f2c70cd10f
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 19:33:53 -0300

    Create main.py

---
+++ Python_Phil_E_and_Federico/5_generators/pharmacy/main.py
@@ -0,0 +1,32 @@
+import tickets
+
+def ask():
+    print("Welcome to Python Drugstore")
+
+    while True:
+        print('[P] - Perfume\n[M] - Medicine\n[C] - Cosmetics')
+        try:
+            my_product = input('Choose your product: ').upper()
+            ['P', 'M', 'C'].index(my_product)
+        except ValueError:
+            print('That is not a valid option')
+        else:
+            break
+
+    tickets.decorator((my_product))
+
+def start():
+
+    while True:
+        ask()
+        try:
+            another_ticket == input('Do you want another ticket? [Y] [N]').upper()
+            ['Y', 'N'].index(another_ticket)
+        except ValueError:
+            print('Not a valid option')
+        else:
+            if another_ticket == 'N':
+                print('Thanks for visiting our Python Drugstore')
+                break
+
+start()
