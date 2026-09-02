5ac7290ed24e77d4df4f98a189f4a2cd505ee8a3
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 16:00:41 -0300

    Generators - Python

---
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -0,0 +1,3 @@
+'''Create a generator (stored in the variable practice_generator) that is capable of returning 
+multiples of 7 indefinitely, starting from 7 itself, and that each time it is called returns the 
+next multiple (7, 14, 21, 28... ).'''
