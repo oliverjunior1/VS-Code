f660f7037831451c72a4fb1a4e287b725950efdc
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 15:32:12 -0300

    Generators - Python

--- Python_Phil_E_and_Federico/5_generators/2_generators.py
+++ Python_Phil_E_and_Federico/5_generators/2_generators.py
@@ -2,18 +2,3 @@
 infinite sequence of numbers, starting from 1, and returning a higher consecutive number each time 
 it is called using next.'''
 
-def infinity_generation():
-    num = 0
-    while True:
-        yield num
-        num += 1
-        
-        
-        
-
-x = infinity_generation()
-
-print(next(x))
-print(next(x))
-print(next(x))
-print(next(x))
