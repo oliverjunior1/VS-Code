eb095d1de578a3b5e2e378dc65c18a8f447bd9cd
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-02 16:18:24 -0300

    Generators - Python

---
+++ Python_Phil_E_and_Federico/5_generators/3_generators.py
@@ -0,0 +1,8 @@
+'''Create a generator that subtracts the lives of a video game character one by one, and returns a 
+message each time it is called:
+"You have 3 lives left"
+"You have 2 lives left"
+"You have 1 live left"
+"Game Over"
+Store the generator in the variable lose_live'''
+
