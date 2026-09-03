eb90e03fc677cdbd259d8ac02996f57b03768179
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-03 19:42:50 -0300

    collections - Python

--- Python_Phil_E_and_Federico/6_collectors/6_datetime.py
+++ Python_Phil_E_and_Federico/6_collectors/6_datetime.py
@@ -1,3 +1,8 @@
 '''In a variable called current_minutes, store only the minutes of the current time.
 For example, if it were executed at 20:43:17, the current_minutes variable should store the value 43'''
 
+from datetime import datetime
+
+current_minutes = datetime.now()
+
+print(current_minutes)
