ab14a6320c21a196589d843aef9f7c890559104e
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-03 18:07:36 -0300

    collections - Python

---
+++ Python_Phil_E_and_Federico/6_collectors/2_collections.py
@@ -0,0 +1,6 @@
+'''Create a dictionary called my_dictionary, for which, when a searched keyword is not found, it is loaded with the string "Value not found".
+Load the dictionary with at least the following data pairs:
+keyword = age
+value = 44
+Use the defaultdict method of the Collections module.'''
+
