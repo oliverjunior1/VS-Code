b065b2264049a6c3179862d4ed0f32d3d8077511
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-03 18:04:42 -0300

    collections - Python

--- Python_Phil_E_and_Federico/6_collectors/1_collections.py
+++ Python_Phil_E_and_Federico/6_collectors/1_collections.py
@@ -1,1 +1,8 @@
 '''Apply a Counter to the list of numbers given below, and store it in a variable called my_counter'''
+from collections import Counter
+
+my_list = [1, 2, 3, 6, 7, 1, 2, 4, 5, 5, 5, 5, 3, 2, 6, 7]
+
+my_counter = Counter(my_list)
+
+print(my_counter)
