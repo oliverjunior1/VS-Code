c782ff00659e819913b0852108f31ea82d40af28
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-03 19:11:14 -0300

    collections - Python

---
+++ Python_Phil_E_and_Federico/6_collectors/4_datetime.py
@@ -0,0 +1,12 @@
+'''A double-ended queue or deque is a linear data structure that allows elements to be inserted and deleted at 
+both ends.
+Find out more about it on any documentation site, and then implement a deque from the collections module. 
+The initial elements of the list are provided below.
+["London", "Berlin", "Paris", "Madrid", "Rome", "Moscow"]
+The list must have the ability to incorporate elements from the left, and be named cities_list.'''
+
+from collections import deque
+
+cities_list = deque(["London", "Berlin", "Paris", "Madrid", "Rome", "Moscow"])
+
+print(cities_list)
