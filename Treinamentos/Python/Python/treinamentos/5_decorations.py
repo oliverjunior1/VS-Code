e12e6543815a553bd9da546f51a04083c45899d8
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-12 10:25:07 -0300

    treinamento python

--- Treinamentos/Python/Python/treinamentos/5_decorations.py
+++ Treinamentos/Python/Python/treinamentos/5_decorations.py
@@ -1,13 +1,2 @@
-def fun1(x):
-    def fun2():
-        print("#############################")
-        x()
-        print("#############################")
-    return fun2
 
-@fun1
-def greetings():
-    print("Jesus is the light of the world!")
-
-greetings()
 
