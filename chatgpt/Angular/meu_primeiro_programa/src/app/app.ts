fc77f18056786e8afcbfbfa77ced637509ac5d98
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-17 15:46:05 -0300

    Update app.ts

--- chatgpt/Angular/meu_primeiro_programa/src/app/app.ts
+++ chatgpt/Angular/meu_primeiro_programa/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
