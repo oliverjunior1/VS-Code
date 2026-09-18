cda1aecc39a27339329c9208a19988f2be245ea6
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-18 12:35:18 -0300

    Create 10th_elseif.php

---
+++ chatgpt/php/1st_day/10th_elseif.php
@@ -0,0 +1,15 @@
+<?php
+
+$nota = 8;
+
+if ($nota >= 9) {
+    echo "Excelente";
+}elseif ($nota >= 7) {
+    echo "Aprovado";
+}elseif ($nota>=5) {
+    echo "Recuperação";
+}else {
+    echo "Reprovado";
+}
+
+?>
