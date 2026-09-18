1ef7e07b925f6ace57c550c99faae77a81ed6105
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-18 16:15:39 -0300

    Update 20th_programa_cliente.php

--- chatgpt/php/1st_day/20th_programa_cliente.php
+++ chatgpt/php/1st_day/20th_programa_cliente.php
@@ -7,23 +7,7 @@ $clientes = [
     "Ana",
     "Carlos"
 ];
-function verificarNome($nome) {
-    if ("$nome" == "João") {
-        echo "Hello $nome";
-    }elseif ("$nome" == "Maria") {
-        echo "Cliente $nome";
-    }elseif ("$nome" == "Pedro") {
-        echo "Hello $nome";
-    }elseif ("$nome" == "Ana") {
-        echo "Hello $nome";
-    } else {
-        echo "Hello $nome";
-    } ;
-}
 
-foreach($clientes as $cliente) {
-    verificarNome($cliente);
-}
 
 
 
