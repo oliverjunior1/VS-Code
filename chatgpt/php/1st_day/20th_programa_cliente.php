91b24926bec82a3fbcbb43f26fcb83b4b92d230a
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-18 18:35:45 -0300

    Update 20th_programa_cliente.php

--- chatgpt/php/1st_day/20th_programa_cliente.php
+++ chatgpt/php/1st_day/20th_programa_cliente.php
@@ -1,13 +1,23 @@
 <?php
 
-$clientes = [
+function verificarNome($nome) {
+    $clientes = [
     "João",
     "Maria",
     "Pedro",
     "Ana",
     "Carlos"
 ];
+    foreach($clientes as $cliente) {
+        if ($nome == $cliente) {
+            echo "Cliente: $cliente <br>, cliente encontrado! <br>";
+        }else {
+            echo "Cliente: $cliente <br>";
+        }
+    }
+}
 
+verificarNome("Maria");
 
 
 
