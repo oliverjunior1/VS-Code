38ec83b00acc7ab5b3c7cbf67c63cb94841f041a
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-19 23:02:13 -0300

    Update index.js

--- chatgpt/html_css_javascript/1st_day/1st_program/index.js
+++ chatgpt/html_css_javascript/1st_day/1st_program/index.js
@@ -4,10 +4,11 @@
 //let idade = 30 // o valor pode mudar
 //const corDoCabelo = "Castanho" //valor imutável
 
-let idade = 25
+/* let idade = 25
 
 if (idade>=18) {
     console.log("Maior de idade")
 }else {
     console.log("Menor de idade")
-}
+} */
+
