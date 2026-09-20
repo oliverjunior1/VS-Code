4d5481aa81e87f8e44d1b6e9d8f5259c93c2c07a
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-19 22:58:59 -0300

    Update index.js

--- chatgpt/html_css_javascript/1st_day/1st_program/index.js
+++ chatgpt/html_css_javascript/1st_day/1st_program/index.js
@@ -1,6 +1,7 @@
 //console.log("Hello")
 
-let nome = "Joaquim"
-let idade = 30 // o valor pode mudar
-const corDoCabelo = "Castanho" //valor imutável
+//let nome = "Joaquim"
+//let idade = 30 // o valor pode mudar
+//const corDoCabelo = "Castanho" //valor imutável
+
 
