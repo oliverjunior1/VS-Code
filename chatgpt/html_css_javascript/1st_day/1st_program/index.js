6454204cd46480d43701002c5038c6e9eb4ee23d
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-20 10:46:03 -0300

    1st day - html, css, javascript

--- chatgpt/html_css_javascript/1st_day/1st_program/index.js
+++ chatgpt/html_css_javascript/1st_day/1st_program/index.js
@@ -1,14 +1,23 @@
 //console.log("Hello")
 
-//let nome = "Joaquim"
-//let idade = 30 // o valor pode mudar
-//const corDoCabelo = "Castanho" //valor imutável
+//Diferença entre let e const
+/* let nome = "Joaquim"
+let idade = 30 // o valor pode mudar
+const corDoCabelo = "Castanho" //valor imutável
+ */
 
+//// If else
 /* let idade = 25
-
 if (idade>=18) {
     console.log("Maior de idade")
 }else {
     console.log("Menor de idade")
 } */
+
+// Function
+/* function saudacao(nome) {
+    return "Olá, " + nome
+}
+
+console.log(saudacao("Joaquim")) */
 
