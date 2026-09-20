d3c1a32d32cb3eac84ee527f72bfeb238d12b28f
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-20 16:25:44 -0300

    1st day - html, css, javascript

--- chatgpt/html_css_javascript/1st_day/1st_program/index.js
+++ chatgpt/html_css_javascript/1st_day/1st_program/index.js
@@ -37,3 +37,14 @@ botao.addEventListener("click", function() {
 }); */
 
 
+const input = document.getElementById("nome");
+const botao = document.getElementById("botao");
+const resultado = document.getElementById("resultado");
+
+botao.addEventListener("click", function() {
+
+    const nome = input.value;
+
+    resultado.textContent = "Olá, " + nome + "!";
+
+});
