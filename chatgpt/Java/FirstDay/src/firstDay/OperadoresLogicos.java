a2c348320b534bef48a34de6e9a143161d156612
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 18:40:03 -0300

    Create OperadoresLogicos.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package firstDay;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		int idade = 25;
+		boolean documento = true;
+		
+		if (idade >= 18 && documento) {
+			System.out.println("Entrada permitida.");
+		}
+
+	}
+
+}
