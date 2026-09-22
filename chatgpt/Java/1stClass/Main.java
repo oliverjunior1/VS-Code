594efd7bc09989e526e4807ee4543a6730143daf
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 16:42:42 -0300

    Update Main.java

--- chatgpt/Java/1stClass/Main.java
+++ chatgpt/Java/1stClass/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
     //System.out.println("Thank you my God!");
 
+    
     //variáveis
     /* String nome = "Joaquim";
     int idade = 30;
@@ -13,10 +16,12 @@ public class Main {
     System.out.println(salario);
     System.out.println(ativo); */
 
+    
     //String
     /* String nome = "Joaquim";
     System.out.println(nome); */
 
+    
     //Concatenar
     /* String nome = "Joaquim";
     int idade = 30;
@@ -24,10 +29,12 @@ public class Main {
     System.out.println("Nome: " + nome);
     System.out.println("Idade: " + idade); */
 
+    
     // int com operações
     /* int idade = 28;
     System.out.println(idade + 1); */
 
+    
     //double
     /* double preco = 50;
     int quantidade = 3;
@@ -36,8 +43,9 @@ public class Main {
 
     System.out.println(total); */
 
+    
     //Exemplo prático 1
-    String nome = "Joaquim";
+    /* String nome = "Joaquim";
     int idade = 35;
     double salario = 5000;
     boolean ativo = true;
@@ -45,6 +53,27 @@ public class Main {
     System.out.println("Nome: " + nome);
     System.out.println("Idade: " + idade);
     System.out.println("Salário: " + salario);
-    System.out.println("Ativo: " + ativo);
+    System.out.println("Ativo: " + ativo); */
+
+    
+    //Operadores matemáticos
+    /* int a = 10;
+    int b = 3;
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b); */
+    
+
+    //receber dados
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite seu nome: ");
+
+    String nome = scanner.nextLine();
+
+    System.out.println("Olá, " + nome);
+
     }
 }
