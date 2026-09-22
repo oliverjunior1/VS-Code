c52970bdc4f8db99d46ea7fdbf9989001b132c1b
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 18:36:01 -0300

    Update ElseIf.java

--- chatgpt/Java/FirstDay/src/firstDay/ElseIf.java
+++ chatgpt/Java/FirstDay/src/firstDay/ElseIf.java
@@ -3,7 +3,20 @@ package firstDay;
 public class ElseIf {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double nota = 8;
+		
+		if (nota >= 9) {
+			System.out.println("Excelente");
+		}
+		else if (nota >= 7) {
+			System.out.println("Aprovado");
+		}
+		else if (nota >= 5) {
+			System.out.println("Recuperação");
+		}
+		else {
+			System.out.println("Reprovado");
+		}
 
 	}
 
