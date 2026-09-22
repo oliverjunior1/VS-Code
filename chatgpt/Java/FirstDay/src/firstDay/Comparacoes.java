7bdf29da273ad4629c6ec7a34a7a70072d40318e
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 18:38:18 -0300

    Create Comparacoes.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/Comparacoes.java
@@ -0,0 +1,14 @@
+package firstDay;
+
+public class Comparacoes {
+
+	public static void main(String[] args) {
+		int idade = 25;
+		
+		if (idade >= 18) {
+			System.out.println("Pode entrar.");
+		}
+		
+	}
+
+}
