b2f1e54f816f40a2c6ae1aae6ed0e3d8fdc69d18
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 18:29:17 -0300

    Create IfElse.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/IfElse.java
@@ -0,0 +1,14 @@
+package firstDay;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		
+		if (idade >= 18) {
+			System.out.println("Maior de idade.");
+		}
+
+	}
+
+}
