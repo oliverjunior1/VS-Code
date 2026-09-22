1966872e34720bd52c9976f626a1a0595cfec744
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 20:04:22 -0300

    Create Arrays.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/Arrays.java
@@ -0,0 +1,16 @@
+package firstDay;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		String[] nomes = {
+				"Ana",
+				"João",
+				"Maria"
+		};
+		
+		System.out.println(nomes[1]);
+
+	}
+
+}
