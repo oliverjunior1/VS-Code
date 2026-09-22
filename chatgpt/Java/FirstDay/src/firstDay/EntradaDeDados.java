c852563e47520cb6b977643136ba94bd3ffdffb7
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 17:58:10 -0300

    Update EntradaDeDados.java

--- chatgpt/Java/FirstDay/src/firstDay/EntradaDeDados.java
+++ chatgpt/Java/FirstDay/src/firstDay/EntradaDeDados.java
@@ -1,9 +1,16 @@
 package firstDay;
+import java.util.Scanner;
 
 public class EntradaDeDados {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
+		
+		String nome = scanner.nextLine();
+		
+		System.out.println("Olá, " + nome);
+		
 
 	}
 
