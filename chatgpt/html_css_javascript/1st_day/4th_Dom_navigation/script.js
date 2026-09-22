98477d51f3079d41259d2b29c142e05d2a0e8d57
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 14:43:25 -0300

    Update script.js

--- chatgpt/html_css_javascript/1st_day/4th_Dom_navigation/script.js
+++ chatgpt/html_css_javascript/1st_day/4th_Dom_navigation/script.js
@@ -1,3 +1,5 @@
 /* document.getElementById("id02") = document.getElementById('id01').innerHTML; */ 
 
+/* document.getElementById("id02").innerHTML = document.getElementById("id01").firstChild.nodeValue; */
 
+document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
