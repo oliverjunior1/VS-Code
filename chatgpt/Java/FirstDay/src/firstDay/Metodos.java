bd6017acaef6c73dd0fed4169921e8f12310eacc
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 20:13:55 -0300

    Create Metodos.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/Metodos.java
@@ -0,0 +1,14 @@
+package firstDay;
+
+public class Metodos {
+    
+    // Método precisa estar dentro da classe
+    public static void saudacao() {
+        System.out.println("Olá!");
+    }
+
+    public static void main(String[] args) {
+        saudacao();
+    }
+}
+
