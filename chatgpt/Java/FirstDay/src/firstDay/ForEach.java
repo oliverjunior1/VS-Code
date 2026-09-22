53d3582e1bddad303aada6ec85d8c3a46cff1665
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-22 19:53:40 -0300

    Create ForEach.java

---
+++ chatgpt/Java/FirstDay/src/firstDay/ForEach.java
@@ -0,0 +1,19 @@
+package firstDay;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		String[] nomes = {
+				"Ana",
+				"João",
+				"Maria",
+				"Pedro"
+		};
+		
+		for (String nome: nomes) {
+			System.out.println(nome);
+		}
+
+	}
+
+}
