5b8b24dd555094b50c0c5c4bb13d1c0d6883a2a3
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-23 20:28:00 -0300

    Update 12_sistema_clientes.py

---
+++ chatgpt/Python/1st_day/12_sistema_clientes.py
@@ -0,0 +1,31 @@
+clientes_dicts = [{
+    "mome":"Ana",
+    "idade":25,
+    "salario":3500,
+    "cidade":"Goiânia"
+},
+{
+    "nome":"Joao",
+    "idade":22,
+    "salario":5000,
+    "cidade": "Anápolis"
+},
+{
+    "nome":"Maria",
+    "idade":50,
+    "salario":4200,
+    "cidade":"Belo Horizonte"
+},
+{
+    "nome":"José",
+    "idade":59,
+    "salario":4900,
+    "cidade":"Brasília"
+},
+{
+    "nome":"Michel",
+    "idade":62,
+    "salario":3200,
+    "cidade":"Rio de Janeiro"
+}]
+
