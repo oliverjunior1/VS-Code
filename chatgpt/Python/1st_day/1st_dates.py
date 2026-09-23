2612644b3708f074269300ed714023d1e1edfbb7
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-23 18:47:43 -0300

    Create 1st_dates.py

---
+++ chatgpt/Python/1st_day/1st_dates.py
@@ -0,0 +1,9 @@
+nome = input("Put your name: ")
+age = int(input("Put your age: "))
+salary = float(input("Put your salary"))
+
+print()
+print("===== CADASTRO =====")
+print("Nome: ", nome)
+print("Idade: ", age)
+print("Salário: ", salary)
