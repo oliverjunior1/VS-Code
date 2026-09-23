765ed9296be5a5e552e4558958043bb4ef69b5fa
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-23 19:54:00 -0300

    Update 11th_juntando_tudo.py

---
+++ chatgpt/Python/1st_day/11th_juntando_tudo.py
@@ -0,0 +1,12 @@
+def classificar_salario(salario):
+
+    if salario >= 5000:
+        return "Alto"
+
+    elif salario >= 3000:
+        return "Médio"
+
+    else:
+        return "Baixo"
+
+    
