92f5af0851ceff02050b146226a9bb2122e7ccd2
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-23 19:59:34 -0300

    Update 11th_juntando_tudo.py

--- chatgpt/Python/1st_day/11th_juntando_tudo.py
+++ chatgpt/Python/1st_day/11th_juntando_tudo.py
@@ -9,4 +9,26 @@ def classificar_salario(salario):
     else:
         return "Baixo"
 
-    
+nome = input("Nome: ")
+
+try:
+    idade = int(input("Idade: "))
+    salario = float(input("Salário: "))
+
+    print()
+    print("===== CLIENTE =====")
+    print("Nome:", nome)
+    print("Idade:", idade)
+    print("Salário:", salario)
+
+    if idade >= 18:
+        print("Maior de idade.")
+    else:
+        print("Menor de idade.")
+
+    categoria = classificar_salario(salario)
+
+    print("Categoria salarial:", categoria)
+
+except ValueError:
+    print("Digite valores numéricos válidos.")
