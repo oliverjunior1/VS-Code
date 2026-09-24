a8b73a2fdd4581bb266a7d85aca6708f37114df2
oliverjunior1 <oliver-junior@hotmail.com>
2026-09-24 12:08:09 -0300

    Update 12_sistema_clientes.py

--- chatgpt/Python/1st_day/12_sistema_clientes.py
+++ chatgpt/Python/1st_day/12_sistema_clientes.py
@@ -1,39 +1,41 @@
-clientes_dicts = [{
-    "mome":"Ana",
-    "idade":25,
-    "salario":3500,
-    "cidade":"Goiânia"
-},
-{
-    "nome":"Joao",
-    "idade":22,
-    "salario":5000,
-    "cidade": "Anápolis"
-},
-{
-    "nome":"Maria",
-    "idade":50,
-    "salario":4200,
-    "cidade":"Belo Horizonte"
-},
-{
-    "nome":"José",
-    "idade":59,
-    "salario":4900,
-    "cidade":"Brasília"
-},
-{
-    "nome":"Michel",
-    "idade":62,
-    "salario":3200,
-    "cidade":"Rio de Janeiro"
-}]
+clientes_dicts = [
+    {
+        "nome": "Ana",
+        "idade": 25,
+        "salario": 3500,
+        "cidade": "Goiânia"
+    },
+    {
+        "nome": "Joao",
+        "idade": 22,
+        "salario": 5000,
+        "cidade": "Anápolis"
+    },
+    {
+        "nome": "Maria",
+        "idade": 50,
+        "salario": 4200,
+        "cidade": "Belo Horizonte"
+    },
+    {
+        "nome": "José",
+        "idade": 59,
+        "salario": 4900,
+        "cidade": "Brasília"
+    },
+    {
+        "nome": "Michel",
+        "idade": 62,
+        "salario": 3200,
+        "cidade": "Rio de Janeiro"
+    }
+]
 
-# 1 mostrar todos os clientes
-print(clientes_dicts)
+# 1. Mostrar todos os clientes
+for cliente in clientes_dicts:
+    print(cliente)
 
-# mostrar apenas maiores de idade
-for chave in clientes_dicts.idade:
-    if chave['idade'] >= 18:
-        print(chave['nome'])
-
+# 2. Mostrar apenas clientes maiores de idade
+for cliente in clientes_dicts:
+    if cliente["idade"] >= 18:
+        print(cliente["nome"])
